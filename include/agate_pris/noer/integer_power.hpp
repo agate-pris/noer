e39b8ce40fab9344333ae0857b31c5f46f498381
agate-pris <agate-pris@users.noreply.github.com>
2015-06-15 20:22:26 +0900

    [fix] インクルードガードを追加
    integer_power.hpp

--- include/agate_pris/noer/integer_power.hpp
+++ include/agate_pris/noer/integer_power.hpp
@@ -1,3 +1,6 @@
+﻿#ifndef AGATE_PRIS_NOER_INTEGER_POWER_HPP
+#define AGATE_PRIS_NOER_INTEGER_POWER_HPP
+
 #include <boost/multiprecision/cpp_int.hpp>
 
 namespace agate_pris
@@ -7,3 +10,5 @@ namespace agate_pris
 		boost::multiprecision::cpp_int integer_power( int m, unsigned int k );
 	}
 }
+
+#endif // AGATE_PRIS_NOER_INTEGER_POWER_HPP
