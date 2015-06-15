75d9c54e37e20c67a031f0912f59efa85c12fa85
agate-pris <agate-pris@users.noreply.github.com>
2015-06-15 21:41:02 +0900

    [add] 階乗を計算する関数を追加
    agate_pris::noer::factorial

---
+++ include/agate_pris/noer/factorial.hpp
@@ -0,0 +1,25 @@
+﻿#ifndef AGATE_PRIS_NOER_FACTORIAL_HPP
+#define AGATE_PRIS_NOER_FACTORIAL_HPP
+
+#include <boost/multiprecision/cpp_int.hpp>
+
+namespace agate_pris
+{
+	namespace noer
+	{
+		template< typename ReturnValue = boost::multiprecision::cpp_int, typename Arg >
+		ReturnValue factorial(Arg arg)
+		{
+			ReturnValue result = arg;
+
+			while (--arg > 0)
+			{
+				result *= arg;
+			}
+
+			return result;
+		}
+	}
+}
+
+#endif // AGATE_PRIS_NOER_FACTORIAL_HPP
