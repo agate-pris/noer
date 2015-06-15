0568419402fb8c35e50ded8dd4f689ee294d5ab4
agate-pris <agate-pris@users.noreply.github.com>
2015-06-15 20:13:31 +0900

    [fix] インクルードガードを追加
    bbp_formula.hpp にインクルードガードを追加

--- include/agate_pris/noer/bbp_formula.hpp
+++ include/agate_pris/noer/bbp_formula.hpp
@@ -1,3 +1,6 @@
+#ifndef AGATE_PRIS_NOER_BBP_FORMULA_HPP
+#define AGATE_PRIS_NOER_BBP_FORMULA_HPP
+
 #include <boost/multiprecision/cpp_int.hpp>
 
 namespace agate_pris
@@ -7,3 +10,5 @@ namespace agate_pris
 		boost::multiprecision::cpp_rational bbp_formula( const unsigned int precision );
 	}
 }
+
+#endif // AGATE_PRIS_NOER_BBP_FORMULA_HPP
