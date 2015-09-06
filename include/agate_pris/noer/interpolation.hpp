5bfb417d6c5144814771f9a3e0cb183f0f52da4e
agate-pris <agate-pris@users.noreply.github.com>
2015-09-06 14:24:30 +0900

    [add] 補間の種類を表す enum class interpolation を追加

---
+++ include/agate_pris/noer/interpolation.hpp
@@ -0,0 +1,17 @@
+﻿
+#ifndef AGATE_PRIS_NOER_INTERPOLATION_HPP
+#define AGATE_PRIS_NOER_INTERPOLATION_HPP
+
+namespace agate_pris
+{
+	namespace noer
+	{
+		enum class interpolation
+		{
+			k_nearest_neighbor,	// 0次補間 最近傍補間
+			linear				// 1次補間 線形補間
+		};
+	}
+}
+
+#endif
