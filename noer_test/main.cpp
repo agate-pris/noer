6f7f66152e5db756b2f5991b3d0acf80e5b52ba2
agate-pris <agate-pris@users.noreply.github.com>
2015-09-06 13:38:50 +0900

    [modify] test main.cpp 名前空間 agate_pris::noer を省略

--- noer_test/main.cpp
+++ noer_test/main.cpp
@@ -10,13 +10,14 @@
 int main()
 {
 	using namespace std;
+	using namespace agate_pris::noer;
 
-	agate_pris::noer::fixed_point_number< 8, int > fpn;
+	fixed_point_number< 8, int > fpn;
 	fpn = fpn / fpn;
 
 	for (int i = 0; i < 10; ++i)
 	{
-		auto result = agate_pris::noer::bbp_formula(i + 1);
+		auto result = bbp_formula(i + 1);
 		cout << fixed << setprecision( 16 ) << result.convert_to< double >() << "(" << result << ")" << endl;
 	}
 
