dcb4b36927ab92920872e318533e9e178bada69b
agate-pris <agate-pris@users.noreply.github.com>
2015-12-16 19:50:16 +0900

    add クラス excess_k を追加

---
+++ include/agate_pris/noer/excess_k.hpp
@@ -0,0 +1,244 @@
+
+#ifndef EXCESS_K_HPP
+#define EXCESS_K_HPP
+
+#include <utility>
+#include <type_traits>
+#include <limits>
+#include <boost/assert.hpp>
+#include <boost/operators.hpp>
+#include <boost/multiprecision/cpp_int.hpp>
+
+namespace agate_pris
+{
+	namespace noer
+	{
+		// excess_k definition
+		// ===================
+		template< typename Repr, unsigned int K >
+		class excess_k
+			: public boost::additive< excess_k< Repr, K > >
+			, public boost::multiplicative< excess_k< Repr, K > >
+			, public boost::equality_comparable< excess_k< Repr, K > >
+			, public boost::less_than_comparable< excess_k< Repr, K > >
+		{
+		private:
+			// member variable
+			// ---------------
+			Repr m_repr;
+
+		public:
+			// constants
+			// ---------
+			static constexpr const int k_offset = static_cast< int >( K );
+
+			// static functions
+			// ----------------
+			template< typename T > static T convertible_max();
+			template< typename T > static T convertible_lowest();
+			template< typename T > static inline T add_offset( T arg );
+			template< typename T > static inline T sub_offset( T arg );
+
+			// constructor
+			// -----------
+			template< typename Arg >
+			explicit excess_k( const Arg& arg );
+
+			// conversion
+			// ----------
+			template< typename T >
+			explicit operator T ()const;
+
+			// binary operator templates
+			// -------------------------
+			template< typename Rhs > excess_k< Repr, K >& operator = ( const Rhs& rhs );
+			template< typename Rhs > excess_k< Repr, K >& operator += ( const Rhs& rhs );
+			template< typename Rhs > excess_k< Repr, K >& operator -= ( const Rhs& rhs );
+			template< typename Rhs > excess_k< Repr, K >& operator *= ( const Rhs& rhs );
+			template< typename Rhs > excess_k< Repr, K >& operator /= ( const Rhs& rhs );
+
+			// binary operator for excess_k< Repr, K >
+			// ---------------------------------------
+			inline bool operator == ( const excess_k< Repr, K >& rhs )const;
+			inline bool operator < ( const excess_k< Repr, K >& rhs )const;
+			excess_k< Repr, K >& operator += ( const excess_k< Repr, K >& rhs );
+			excess_k< Repr, K >& operator -= ( const excess_k< Repr, K >& rhs );
+			excess_k< Repr, K >& operator *= ( const excess_k< Repr, K >& rhs );
+			excess_k< Repr, K >& operator /= ( const excess_k< Repr, K >& rhs );
+
+			// other public functions
+			// ----------------------
+			inline const Repr& get()const{ return m_repr; }
+			inline       Repr& get()     { return m_repr; }
+		};
+
+		// static functions
+		// ----------------
+
+		// get max value convertible from T to excess_k< Repr, K >
+		template< typename Repr, unsigned int K >
+		template< typename T >
+		T excess_k< Repr, K >::convertible_max()
+		{
+			using std::numeric_limits;
+			auto max = numeric_limits< T >::max();
+			return max -= k_offset;
+		}
+
+		// get lowest value convertible from excess_k< Repr, K > to T
+		template< typename Repr, unsigned int K >
+		template< typename T >
+		T excess_k< Repr, K >::convertible_lowest()
+		{
+			using std::numeric_limits;
+			auto lowest = numeric_limits< T >::lowest();
+			return lowest += k_offset;
+		}
+
+		template< typename Repr, unsigned int K >
+		template< typename T >
+		inline T excess_k< Repr, K >::add_offset( T arg )
+		{
+			BOOST_ASSERT( arg <= convertible_max< T >() );
+			return arg += k_offset;
+		}
+
+		template< typename Repr, unsigned int K >
+		template< typename T >
+		inline T excess_k< Repr, K >::sub_offset( T arg )
+		{
+			BOOST_ASSERT( convertible_lowest< T >() <= arg );
+			return arg -= k_offset;
+		}
+
+		// constructor
+		// -----------
+		template< typename Repr, unsigned int K >
+		template< typename Arg >
+		excess_k< Repr, K >::excess_k( const Arg& arg )
+			: m_repr( static_cast< Repr >( add_offset( arg ) ) )
+		{
+		}
+
+		// conversion
+		// ----------
+		template< typename Repr, unsigned int K >
+		template< typename T >
+		excess_k< Repr, K >::operator T ()const
+		{
+			return sub_offset( static_cast< T >( m_repr ) );
+		}
+
+		// binary operator templates
+		// -------------------------
+
+		// operator =
+		template< typename Repr, unsigned int K >
+		template< typename Rhs >
+		excess_k< Repr, K >&
+			excess_k< Repr, K >::operator = ( const Rhs& rhs )
+		{
+			m_repr = static_cast< Repr >( add_offset( rhs ) );
+			return *this;
+		}
+
+		// operator +=
+		template< typename Repr, unsigned int K >
+		template< typename Rhs >
+		excess_k< Repr, K >& excess_k< Repr, K >::operator += ( const Rhs& rhs )
+		{
+			m_repr += rhs;
+			return *this;
+		}
+
+		// operator -=
+		template< typename Repr, unsigned int K >
+		template< typename Rhs >
+		excess_k< Repr, K >& excess_k< Repr, K >::operator -= ( const Rhs& rhs )
+		{
+			m_repr -= rhs;
+			return *this;
+		}
+
+		// operator *=
+		template< typename Repr, unsigned int K >
+		template< typename Rhs >
+		excess_k< Repr, K >& excess_k< Repr, K >::operator *= ( const Rhs& rhs )
+		{
+			auto repr = static_cast< Repr >( *this );
+			repr *= rhs;
+			*this = repr;
+			return *this;
+		}
+
+		// operator /=
+		template< typename Repr, unsigned int K >
+		template< typename Rhs >
+		excess_k< Repr, K >& excess_k< Repr, K >::operator /= ( const Rhs& rhs )
+		{
+			auto repr = static_cast< Repr >( *this );
+			repr /= rhs;
+			*this = repr;
+			return *this;
+		}
+
+		// binary operator for excess_k< Repr, K >
+		// ---------------------------------------
+
+		// operator ==
+		template< typename Repr, unsigned int K >
+		inline bool excess_k< Repr, K >::operator == ( const excess_k< Repr, K >& rhs )const
+		{
+			return m_repr == rhs.get();
+		}
+
+		// operator <
+		template< typename Repr, unsigned int K >
+		inline bool excess_k< Repr, K >::operator < ( const excess_k< Repr, K >& rhs )const
+		{
+			return m_repr < rhs.get();
+		}
+
+		// operator +=
+		template< typename Repr, unsigned int K >
+		excess_k< Repr, K >& excess_k< Repr, K >::operator += ( const excess_k< Repr, K >& rhs )
+		{
+			using std::numeric_limits;
+			BOOST_ASSERT( m_repr <= numeric_limits< Repr >::max() - rhs.get() );
+			m_repr += rhs.get();
+			m_repr -= k_offset;
+			return *this;
+		}
+
+		// operator -=
+		template< typename Repr, unsigned int K >
+		excess_k< Repr, K >& excess_k< Repr, K >::operator -= ( const excess_k< Repr, K >& rhs )
+		{
+			using std::numeric_limits;
+			BOOST_ASSERT( m_repr <= convertible_max< Repr >() );
+			m_repr += k_offset;
+			m_repr -= rhs.get();
+			return *this;
+		}
+
+		// operator *=
+		template< typename Repr, unsigned int K >
+		excess_k< Repr, K >& excess_k< Repr, K >::operator *= ( const excess_k< Repr, K >& rhs )
+		{
+			auto l = static_cast< Repr >( *this );
+			auto r = static_cast< Repr >( rhs );
+			*this = l * r;
+			return *this;
+		}
+
+		// operator /=
+		template< typename Repr, unsigned int K >
+		excess_k< Repr, K >& excess_k< Repr, K >::operator /= ( const excess_k< Repr, K >& rhs )
+		{
+			m_repr /= static_cast< Repr >( rhs );
+			return *this;
+		}
+	}
+}
+
+#endif // EXCESS_K_HPP
--- noer_test/main.cpp
+++ noer_test/main.cpp
@@ -1,10 +1,16 @@
 #include <iostream>
 #include <boost/multiprecision/cpp_int.hpp>
 #include <Windows.h>
+
+#undef max
+#undef min
+
 #include <agate_pris/noer/pow.hpp>
 #include <agate_pris/noer/bbp_formula.hpp>
 #include <agate_pris/noer/fixed_point_number.hpp>
 #include <agate_pris\noer\degree.hpp>
+#include <agate_pris/noer/excess_k.hpp>
+
 
 #include "factorial.hpp"
 
@@ -12,6 +18,10 @@ int main()
 {
 	using namespace std;
 	using namespace agate_pris::noer;
+
+	excess_k< unsigned int, 1024 > e( 100 );
+	cout << static_cast< int >( e ) << endl;
+	cout << static_cast< int >( e.get() ) << endl;
 
 	fixed_point_number< 8, int > fpn;
 	fpn = fpn / fpn;
