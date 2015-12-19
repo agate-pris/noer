1d4d3c40e39ddc1bbfbd00d3b74645ddd16e9dc7
agate-pris <agate-pris@users.noreply.github.com>
2015-12-19 18:38:48 +0900

    [add] sin_table のテストを追加

---
+++ noer_test/sin_table.hpp
@@ -0,0 +1,28 @@
+
+#define NOMINMAX
+
+#include <iostream>
+#include <iomanip>
+
+#include <agate_pris/noer/bbp_formula.hpp>
+#include <agate_pris/noer/sin_table.hpp>
+
+int main()
+{
+	using namespace std;
+	using namespace agate_pris::noer;
+
+	sin_table< double, 256 > st( bbp_formula( 10 ), 10 );
+
+	for( int i = 0; i <= 24; ++i )
+	{
+		auto d = 65536 * i / 24;
+		cout << right << fixed << setprecision( 2 );
+		cout << "sin( ";
+		cout << setw( 5 ) << d;
+		cout << " / 32768 pi rad ) = ";
+		cout << setw( 5 ) << st.get< 256 >( d ) << endl;
+	}
+
+	system( "pause" );
+}
