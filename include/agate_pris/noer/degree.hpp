d08aef085b98174243665e2abbe2f8d78d2b007f
agate-pris <agate-pris@users.noreply.github.com>
2015-12-22 20:01:06 +0900

    [mod] degree テンプレート引数及びメンバ変数の名前を各々 Magnitude m_magnitude から Quantity m_quantity に変更

--- include/agate_pris/noer/degree.hpp
+++ include/agate_pris/noer/degree.hpp
@@ -10,31 +10,31 @@ namespace agate_pris
 {
 	namespace noer
 	{
-		// Magnitude : 角度の大きさを保持するための型です
+		// Quantity : 角度の大きさを保持するための型です
 		// Precision : 角度の大きさの精度です。一周を2のPrecision乗に分割します。
-		template< typename Magnitude, unsigned int Precision >
+		template< typename Quantity, unsigned int Precision >
 		class degree
 		{
-			static_assert(std::is_integral< Magnitude >::value, "degree::magnitude_type は整数型でなければなりません。あるいは、degreeを特殊化する必要があります。");
+			static_assert(std::is_integral< Quantity >::value, "degree::quantity_type は整数型でなければなりません。あるいは、degreeを特殊化する必要があります。");
 			static_assert(3 < Precision, "Precisionは3より大きい値でなければなりません。");
 		private:
-			Magnitude m_magnitude;
+			Quantity m_quantity;
 		public:
-			static constexpr const Magnitude k_pi   = pow< Magnitude, 2, Precision - 1 >();
-			static constexpr const Magnitude k_pi_2 = pow< Magnitude, 2, Precision - 2 >();
-			static constexpr const Magnitude k_pi_4 = pow< Magnitude, 2, Precision - 3 >();
+			static constexpr const Quantity k_pi   = pow< Quantity, 2, Precision - 1 >();
+			static constexpr const Quantity k_pi_2 = pow< Quantity, 2, Precision - 2 >();
+			static constexpr const Quantity k_pi_4 = pow< Quantity, 2, Precision - 3 >();
 
-			inline const Magnitude& get()const
+			inline const Quantity& get()const
 			{
-				return m_magnitude;
+				return m_quantity;
 			}
-			inline Magnitude& get()
+			inline Quantity& get()
 			{
-				return m_magnitude;
+				return m_quantity;
 			}
 			inline void normalize()
 			{
-				m_magnitude %= k_pi;
+				m_quantity %= k_pi;
 			}
 		};
 	}
