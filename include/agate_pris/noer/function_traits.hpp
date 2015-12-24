52d75222ca51d2f9ab2c97ae37db27a89262bc94
agate-pris <agate-pris@users.noreply.github.com>
2015-12-24 20:27:49 +0900

    [mod] function_traits VC++のvariadic templatesに関連するオーバーロード解決の仕様によって、引数の型が1つの場合と2つ以上の場合のオーバーロード解決が出来ない不具合を修正。

--- include/agate_pris/noer/function_traits.hpp
+++ include/agate_pris/noer/function_traits.hpp
@@ -14,30 +14,30 @@ namespace agate_pris
 		{
 			template< typename P > struct function_pointer
 			{
-				template< typename R                               > static auto argument( R( * )( void       ) ) -> void;
-				template< typename R                               > static auto result(   R( * )( void       ) ) -> void;
-				template< typename R, typename A                   > static auto argument( R( * )( A          ) ) -> A;
-				template< typename R, typename A                   > static auto result(   R( * )( A          ) ) -> R;
-				template< typename R, typename A, typename... Args > static auto argument( R( * )( A, Args... ) ) -> std::tuple< A, Args... >;
-				template< typename R, typename A, typename... Args > static auto result(   R( * )( A, Args... ) ) -> R;
+				template< typename R                                                > static auto argument( R( * )( void          ) ) -> void;
+				template< typename R                                                > static auto result(   R( * )( void          ) ) -> void;
+				template< typename R, typename A                                    > static auto argument( R( * )( A             ) ) -> A;
+				template< typename R, typename A                                    > static auto result(   R( * )( A             ) ) -> R;
+				template< typename R, typename A, typename B, typename... Args      > static auto argument( R( * )( A, B, Args... ) ) -> std::tuple< A, B, Args... >;
+				template< typename R, typename A, typename B, typename... Args      > static auto result(   R( * )( A, B, Args... ) ) -> R;
 				using argument_type = decltype( argument( std::declval< P >() ) );
 				using result_type   = decltype( result(   std::declval< P >() ) );
 			};
 
 			template< typename P > struct member_function_pointer
 			{
-				template< typename R, typename C                               > static auto argument( R( C::* )( void       )      ) -> void;
-				template< typename R, typename C                               > static auto result(   R( C::* )( void       )      ) -> R;
-				template< typename R, typename C                               > static auto argument( R( C::* )( void       )const ) -> void;
-				template< typename R, typename C                               > static auto result(   R( C::* )( void       )const ) -> R;
-				template< typename R, typename C, typename A                   > static auto argument( R( C::* )( A          )      ) -> A;
-				template< typename R, typename C, typename A                   > static auto result(   R( C::* )( A          )      ) -> R;
-				template< typename R, typename C, typename A                   > static auto argument( R( C::* )( A          )const ) -> A;
-				template< typename R, typename C, typename A                   > static auto result(   R( C::* )( A          )const ) -> R;
-				template< typename R, typename C, typename A, typename... Args > static auto argument( R( C::* )( A, Args... )      ) -> std::tuple< A, Args... >;
-				template< typename R, typename C, typename A, typename... Args > static auto result(   R( C::* )( A, Args... )      ) -> R;
-				template< typename R, typename C, typename A, typename... Args > static auto argument( R( C::* )( A, Args... )const ) -> std::tuple< A, Args... >;
-				template< typename R, typename C, typename A, typename... Args > static auto result(   R( C::* )( A, Args... )const ) -> R;
+				template< typename R, typename C                                           > static auto argument( R( C::* )( void       )      ) -> void;
+				template< typename R, typename C                                           > static auto result(   R( C::* )( void       )      ) -> R;
+				template< typename R, typename C                                           > static auto argument( R( C::* )( void       )const ) -> void;
+				template< typename R, typename C                                           > static auto result(   R( C::* )( void       )const ) -> R;
+				template< typename R, typename C, typename A                               > static auto argument( R( C::* )( A          )      ) -> A;
+				template< typename R, typename C, typename A                               > static auto result(   R( C::* )( A          )      ) -> R;
+				template< typename R, typename C, typename A                               > static auto argument( R( C::* )( A          )const ) -> A;
+				template< typename R, typename C, typename A                               > static auto result(   R( C::* )( A          )const ) -> R;
+				template< typename R, typename C, typename A, typename B, typename... Args > static auto argument( R( C::* )( A, B, Args... )      ) -> std::tuple< A, B, Args... >;
+				template< typename R, typename C, typename A, typename B, typename... Args > static auto result(   R( C::* )( A, B, Args... )      ) -> R;
+				template< typename R, typename C, typename A, typename B, typename... Args > static auto argument( R( C::* )( A, B, Args... )const ) -> std::tuple< A, B, Args... >;
+				template< typename R, typename C, typename A, typename B, typename... Args > static auto result(   R( C::* )( A, B, Args... )const ) -> R;
 				using argument_type = decltype( argument( std::declval< P >() ) );
 				using result_type   = decltype( result(   std::declval< P >() ) );
 			};
