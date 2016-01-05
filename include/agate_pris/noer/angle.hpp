e4cf8a5f619d159d5dea370b23e59d36cddd3574
agate-pris <agate-pris@users.noreply.github.com>
2016-01-05 20:07:00 +0900

    [add] angle にデフォルトコンストラクタを追加

--- include/agate_pris/noer/angle.hpp
+++ include/agate_pris/noer/angle.hpp
@@ -17,12 +17,13 @@ namespace agate_pris
 		{
 			static_assert(std::is_integral< Quantity >::value, "angle::quantity_type は整数型でなければなりません。あるいは、angleを特殊化する必要があります。");
 			static_assert(3 < Precision, "Precisionは3より大きい値でなければなりません。");
-		private:
+			private:
 			Quantity m_quantity;
 		public:
 			static constexpr const Quantity k_full     = pow< Quantity, 2, Precision >();
 			static constexpr const Quantity k_straight = pow< Quantity, 2, Precision - 1 >();
 			static constexpr const Quantity k_right    = pow< Quantity, 2, Precision - 2 >();
+			angle() = default;
 
 			// explicit constructor
 			explicit angle( const Quantity& q ) : m_quantity( q ) {}
