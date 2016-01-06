c6bc3c2baa8d52ede75d918dca58eec0a933044a
agate-pris <agate-pris@users.noreply.github.com>
2016-01-06 22:30:44 +0900

    [add] 角度のジェネリックプログラミングを行うためのクラス angle_traits を追加

---
+++ include/agate_pris/noer/angle_traits.hpp
@@ -0,0 +1,13 @@
+
+#ifndef AGATE_PRIS_NOER_ANGLE_TRAITS_HPP
+#define AGATE_PRIS_NOER_ANGLE_TRAITS_HPP
+
+namespace agate_pris
+{
+	namespace noer
+	{
+		template< typename Angle > struct angle_traits;
+	}
+}
+
+#endif
