303f8d62d705ff646c746b844742e1fe68efbf67
agate-pris <agate-pris@users.noreply.github.com>
2016-01-10 12:41:54 +0900

    [add] クラステンプレート exact_number を追加

--- include/agate_pris/noer/exact_number.hpp
+++ include/agate_pris/noer/exact_number.hpp
@@ -1,4 +1,36 @@
-
+﻿
 #ifndef AGATE_PRIS_NOER_EXACT_NUMBER_HPP
 #define AGATE_PRIS_NOER_EXACT_NUMBER_HPP
+
+namespace agate_pris
+{
+	namespace noer
+	{
+		/// @class exact_number
+
+		/// @brief \~japanese 厳密な数値を取り扱うためのクラステンプレート
+		///        \~english  class template for traiting exact numbers
+
+		/// @attention \~japanese `numeric_limits< Repr >::is_exact` は `true` でなければならない。
+		///            \~english  `numeric_limits< Repr >::is_exact` must be `true` .
+
+		/// @tparam Repr \~japanese 内部で保持する値を表現する型
+		///              \~english  represent number as implemention
+
+		/// @details \~japanese `exact_number` は以下のように使う  
+		///          \~english  Use `exact_number` like this
+		///          \~
+		/// ~~~{.cpp}
+		/// exact_number< int > v;
+		/// ~~~
+		template< typename Repr >
+		class exact_number
+		{
+			private:
+			Repr m_repr;
+			public:
+		};
+	}
+}
+
 #endif
