09a8b95b53e7de989bc3684e31c64a5e699c009b
agate-pris <agate-pris@users.noreply.github.com>
2016-01-11 14:51:19 +0900

    [add] 構造体 fraction_traits を追加

--- include/agate_pris/noer/fraction_traits.hpp
+++ include/agate_pris/noer/fraction_traits.hpp
@@ -1,4 +1,13 @@
 
 #ifndef AGATE_PRIS_NOER_FRACTION_TRAITS_HPP
 #define AGATE_PRIS_NOER_FRACTION_TRAITS_HPP
+
+namespace agate_pris
+{
+	namespace noer
+	{
+		template< typename Fraction > struct fraction_traits;
+	}
+}
+
 #endif
