cadc191202f79b99c9896c9f549858a2de76c711
agate-pris <agate-pris@users.noreply.github.com>
2016-01-11 20:50:50 +0900

    [add] 任意の型に対して、静的メンバ変数 value を取得する関数 get を追加

--- include/agate_pris/noer/get.hpp
+++ include/agate_pris/noer/get.hpp
@@ -1,4 +1,17 @@
 
 #ifndef AGATE_PRIS_NOER_GET_HPP
 #define AGATE_PRIS_NOER_GET_HPP
+
+namespace agate_pris
+{
+	namespace noer
+	{
+		template< typename T >
+		inline auto& get()
+		{
+			return T::value;
+		}
+	}
+}
+
 #endif
