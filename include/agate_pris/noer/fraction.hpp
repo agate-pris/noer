54d9a9509ad381302ea2128f1684c1cc2bdafdc8
agate-pris <agate-pris@users.noreply.github.com>
2016-01-11 16:32:09 +0900

    [add] クラス fraction の基本的な構造を作成

--- include/agate_pris/noer/fraction.hpp
+++ include/agate_pris/noer/fraction.hpp
@@ -1,4 +1,41 @@
 
 #ifndef AGATE_PRIS_NOER_FRACTION_HPP
 #define AGATE_PRIS_NOER_FRACTION_HPP
+
+#include <type_traits>
+
+namespace agate_pris
+{
+	namespace noer
+	{
+		namespace detail
+		{
+			namespace fraction
+			{
+				template< typename Repr, typename = std::is_empty< Repr > >
+				struct holder;
+
+				template< typename Repr >
+				struct holder< Repr, std::true_type > : Repr {};
+
+				template< typename Repr >
+				struct holder< Repr, std::false_type >
+				{
+					Repr value;
+				};
+
+				template< typename Repr > struct numerator   : holder< Repr > {};
+				template< typename Repr > struct denominator : holder< Repr > {};
+			}
+		}
+
+		template< typename Numerator, typename Denominator >
+		class fraction 
+		: detail::fraction::numerator< Numerator >
+		, detail::fraction::denominator< Denominator >
+		{
+		};
+	}
+}
+
 #endif
