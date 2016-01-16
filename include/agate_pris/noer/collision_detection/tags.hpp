629312f991633fec4edf6ba23d439a86ffa07ef7
agate-pris <agate-pris@users.noreply.github.com>
2016-01-16 23:07:21 +0900

    [add] collidion_detection::tag と、使用するタグを追加

---
+++ include/agate_pris/noer/collision_detection/tags.hpp
@@ -0,0 +1,21 @@
+
+#ifndef AGATE_PRIS_NOER_COLLISION_DETECTION_TAGS_HPP
+#define AGATE_PRIS_NOER_COLLISION_DETECTION_TAGS_HPP
+
+#include <boost/geometry/core/tags.hpp>
+
+namespace agate_pris
+{
+	namespace noer
+	{
+		namespace collision_detection
+		{
+			using point_tag   = boost::geometry::point_tag;
+			using box_tag     = boost::geometry::box_tag;
+			using segment_tag = boost::geometry::segment_tag;
+			struct triangle_tag {};
+		}
+	}
+}
+
+#endif
