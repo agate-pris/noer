d3923540fa35f01b460e7300caf67847e1ddc419
agate-pris <agate-pris@users.noreply.github.com>
2016-01-20 22:46:54 +0900

    [add] 関数 collision_detection::get collision_detection::set を追加

---
+++ include/agate_pris/noer/collision_detection/access.hpp
@@ -0,0 +1,49 @@
+
+#ifndef AGATE_PRIS_NOER_COLLISION_DETECTION_GET_HPP
+#define AGATE_PRIS_NOER_COLLISION_DETECTION_GET_HPP
+
+#include <utility>
+#include <boost/geometry/core/coordinate_type.hpp>
+#include <boost/geometry/core/access.hpp>
+
+namespace agate_pris
+{
+	namespace noer
+	{
+		namespace collision_detection
+		{
+			namespace traits
+			{
+				template< typename CollisionDetection, std::size_t Index, std::size_t Dimension >
+				struct indexed_access
+				{
+					using coordinate_type = typename boost::geometry::coordinate_type< CollisionDetection >::type;
+
+					static inline coordinate_type get( CollisionDetection const& cd )
+					{
+						return boost::geometry::get< Index, Dimension >( cd );
+					}
+
+					static inline void set( CollisionDetection& cd, coordinate_type const& value )
+					{
+						boost::geometry::set< Index, Dimension >( cd, value );
+					}
+				};
+			}
+			
+			template< std::size_t Index, std::size_t Dimension, typename CollisionDetectionType >
+			auto get( const CollisionDetectionType& cd )
+			{
+				return traits::indexed_access< CollisionDetectionType, Index, Dimension >::get( cd );
+			}
+
+			template< std::size_t Index, std::size_t Dimension, typename CollisionDetectionType, typename CoordinateType >
+			void set( const CollisionDetectionType& cd, const CoordinateType& value )
+			{
+				traits::indexed_access< CollisionDetectionType, Index, Dimension >::set( cd, value );
+			}
+		}
+	}
+}
+
+#endif
