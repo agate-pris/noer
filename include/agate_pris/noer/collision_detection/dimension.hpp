9c884eed308f68e7223282844f8ea092ef25e2fa
agate-pris <agate-pris@users.noreply.github.com>
2016-01-20 22:09:44 +0900

    [add] collision_detection::dimension を追加

---
+++ include/agate_pris/noer/collision_detection/dimension.hpp
@@ -0,0 +1,25 @@
+
+#ifndef AGATE_PRIS_NOER_COLLISION_DETECTION_DIMENSION_HPP
+#define AGATE_PRIS_NOER_COLLISION_DETECTION_DIMENSION_HPP
+
+#include <utility>
+#include <agate_pris/noer/collision_detection/point_type.hpp>
+#include <boost/geometry/core/coordinate_dimension.hpp>
+
+namespace agate_pris
+{
+	namespace noer
+	{
+		namespace collision_detection
+		{
+			template< typename CollisionDetectionType >
+			struct dimension
+			{
+				static constexpr const std::size_t value =
+				boost::geometry::dimension< point_type_t< CollisionDetectionType > >::value;
+			};
+		}
+	}
+}
+
+#endif
