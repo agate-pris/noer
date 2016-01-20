03d68dfee2049680b5e4f4913bb9ed10566f911a
agate-pris <agate-pris@users.noreply.github.com>
2016-01-20 22:08:07 +0900

    [add] collision_detection::point_type を追加

---
+++ include/agate_pris/noer/collision_detection/point_type.hpp
@@ -0,0 +1,33 @@
+
+#ifndef AGATE_PRIS_NOER_COLLISION_DETECTION_POINT_TYPE_HPP
+#define AGATE_PRIS_NOER_COLLISION_DETECTION_POINT_TYPE_HPP
+
+#include <boost/geometry/core/point_type.hpp>
+
+namespace agate_pris
+{
+	namespace noer
+	{
+		namespace collision_detection
+		{
+			namespace traits
+			{
+				template< typename CollisionDetection >
+				struct point_type
+				{
+					using type = typename boost::geometry::point_type< CollisionDetection >::type;
+				};
+			}
+
+			template< typename CollisionDetectionType >
+			struct point_type
+			{
+				using type = typename traits::point_type< CollisionDetectionType >::type;
+			};
+			template< typename CollisionDetectionType >
+			using point_type_t = typename point_type< CollisionDetectionType >::type;
+		}
+	}
+}
+
+#endif
