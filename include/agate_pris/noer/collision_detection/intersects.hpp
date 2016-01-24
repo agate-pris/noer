a7f57d242fbef9968ec7c8f2f790eb66c6b95055
agate-pris <agate-pris@users.noreply.github.com>
2016-01-24 19:42:38 +0900

    Add function intersects for container as collision detection and the other type
    Add three function overloads of intersects.
    - Both arguments are container object
    - One side is container and the other is any type except for polymorphic collision detection type

--- include/agate_pris/noer/collision_detection/intersects.hpp
+++ include/agate_pris/noer/collision_detection/intersects.hpp
@@ -7,6 +7,7 @@
 #include <agate_pris/noer/collision_detection/intersects_box_point.hpp>
 #include <agate_pris/noer/collision_detection/intersects_box_segment.hpp>
 #include <agate_pris/noer/collision_detection/intersects_box_triangle.hpp>
+#include <agate_pris/noer/collision_detection/intersects_container.hpp>
 #include <agate_pris/noer/collision_detection/intersects_point_triangle.hpp>
 #include <agate_pris/noer/collision_detection/intersects_segment_segment.hpp>
 #include <agate_pris/noer/collision_detection/intersects_segment_triangle.hpp>
