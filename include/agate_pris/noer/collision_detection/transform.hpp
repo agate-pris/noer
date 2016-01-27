0a4f954849782f9bd62191c50c99457eecbf3a02
agate-pris <agate-pris@users.noreply.github.com>
2016-01-27 20:01:08 +0900

    Add funcion transform to avoid Boost.Geometry concept check
    collision_detection::transform defaultly call boost::geometry::transform.
    Overload this function and use it
    when Boost.Geometry's concept check is not suitable.

---
+++ include/agate_pris/noer/collision_detection/transform.hpp
@@ -0,0 +1,31 @@
+﻿
+#ifndef AGATE_PRIS_NOER_COLLISION_DETECTION_TRANSFORM_HPP
+#define AGATE_PRIS_NOER_COLLISION_DETECTION_TRANSFORM_HPP
+
+#include <boost/geometry/algorithms/transform.hpp>
+
+namespace agate_pris
+{
+    namespace noer
+    {
+        namespace collision_detection
+        {
+            /// @brief \~english transform object from source to target
+            ///        \~japanese ソースからターゲットへオブジェクトを変換します。
+            /// @details \~english defaultly call `boost::geometry::transform` .
+            /// When Boost.Geometry cannnot traits object well,
+            /// overload this function and call it.
+            /// \~japanese デフォルトでは `boost::geometry::transform` を呼び出します。
+            /// Boost.Geometryが上手くオブジェクトを扱えない時、
+            /// この関数をオーバロードして使用します。
+            // when Boost.Geometry cannnot traits well.
+            template< typename Source, typename Target >
+            bool transform( Source const& s, Target& t )
+            {
+                return boost::geometry::transform( s, t );
+            }
+        }
+    }
+}
+
+#endif
