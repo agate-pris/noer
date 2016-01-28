5a76637c38ce52d00d4f61a9b903105390c8d6b0
agate-pris <agate-pris@users.noreply.github.com>
2016-01-29 00:56:29 +0900

    Remove specialization noer::numeric_limits for fixed_point

--- include/agate_pris/noer/fixed_point.hpp
+++ include/agate_pris/noer/fixed_point.hpp
@@ -11,7 +11,6 @@
 #include <boost/multiprecision/cpp_int.hpp>
 #include <agate_pris/noer/eval.hpp>
 #include <agate_pris/noer/pow.hpp>
-#include <agate_pris/noer/numeric_limits.hpp>
 
 namespace agate_pris
 {
@@ -89,12 +88,6 @@ namespace agate_pris
 			// ----------------------
 			inline const Repr& get()const{ return m_repr; }
 			inline       Repr& get()     { return m_repr; }
-		};
-
-		template< typename Repr, int Exp >
-		struct numeric_limits< fixed_point< Repr, Exp > >
-		{
-			static constexpr const bool is_exact = numeric_limits< Repr >::is_exact;
 		};
 
 		// static functions
