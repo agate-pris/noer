0ee3ee66ea39c508c804403d0358b5492873a0ee
agate-pris <agate-pris@users.noreply.github.com>
2016-02-01 21:50:49 +0900

    Change point to derive boost::numeric::ublas::vector

--- include/agate_pris/noer/collision_detection/point.hpp
+++ include/agate_pris/noer/collision_detection/point.hpp
@@ -11,153 +11,150 @@
 
 namespace agate_pris
 {
-	namespace noer
-	{
-		namespace collision_detection
-		{
-			namespace detail
-			{
-				template< std::size_t DimensionCount, std::size_t Index >
-				struct array_assign
-				{
-					template< typename CoordinateType, typename ArrayType >
-					static inline void apply( boost::numeric::ublas::vector< CoordinateType, ArrayType >& values, CoordinateType const& value )
-					{
-						values( Index ) = value;
-					}
-				};
+    namespace noer
+    {
+        namespace collision_detection
+        {
+            namespace detail
+            {
+                template< std::size_t DimensionCount, std::size_t Index >
+                struct array_assign
+                {
+                    template< typename CoordinateType, typename ArrayType >
+                    static inline void apply( boost::numeric::ublas::vector< CoordinateType, ArrayType >& values, CoordinateType const& value )
+                    {
+                        values( Index ) = value;
+                    }
+                };
 
-				template<> struct array_assign< 2, 2 >
-				{
-					template< typename CoordinateType, typename ArrayType >
-					static inline void apply( boost::numeric::ublas::vector< CoordinateType, ArrayType >&, CoordinateType const& ) {}
-				};
+                template<> struct array_assign< 2, 2 >
+                {
+                    template< typename CoordinateType, typename ArrayType >
+                    static inline void apply( boost::numeric::ublas::vector< CoordinateType, ArrayType >&, CoordinateType const& ) {}
+                };
 
-				template<> struct array_assign< 1, 1 >
-				{
-					template< typename CoordinateType, typename ArrayType >
-					static inline void apply( boost::numeric::ublas::vector< CoordinateType, ArrayType >&, CoordinateType const& ) {}
-				};
+                template<> struct array_assign< 1, 1 >
+                {
+                    template< typename CoordinateType, typename ArrayType >
+                    static inline void apply( boost::numeric::ublas::vector< CoordinateType, ArrayType >&, CoordinateType const& ) {}
+                };
 
-				template<> struct array_assign< 1, 2 >
-				{
-					template< typename CoordinateType, typename ArrayType >
-					static inline void apply( boost::numeric::ublas::vector< CoordinateType, ArrayType >&, CoordinateType const& ) {}
-				};
-			}
+                template<> struct array_assign< 1, 2 >
+                {
+                    template< typename CoordinateType, typename ArrayType >
+                    static inline void apply( boost::numeric::ublas::vector< CoordinateType, ArrayType >&, CoordinateType const& ) {}
+                };
+            }
 
-			namespace point_hide
-			{
-				template
-				<
-					typename CoordinateType,
-					std::size_t DimensionCount,
-					typename ArrayType = boost::numeric::ublas::bounded_array
-					<
-						CoordinateType,
-						DimensionCount
-					>
-				>
-				class point
-				{
-					private:
-					boost::numeric::ublas::vector< CoordinateType, ArrayType > m_values;
+            namespace point_hide
+            {
+                template
+                <
+                    typename CoordinateType,
+                    std::size_t DimensionCount,
+                    typename ArrayType = boost::numeric::ublas::bounded_array
+                    <
+                        CoordinateType,
+                        DimensionCount
+                    >
+                >
+                class point : public boost::numeric::ublas::vector< CoordinateType, ArrayType >
+                {
+                    public:
+                    point()
+                    : boost::numeric::ublas::vector< CoordinateType, ArrayType >( DimensionCount )
+                    {}
 
-					public:
-					point()
-					: m_values( DimensionCount )
-					{}
+                    /// @brief Constructor to set one value
+                    explicit inline point( CoordinateType const& v0 )
+                    : boost::numeric::ublas::vector< CoordinateType, ArrayType >( DimensionCount )
+                    {
+                        detail::array_assign< DimensionCount, 0 >::apply( *this, v0 );
+                        detail::array_assign< DimensionCount, 1 >::apply( *this, CoordinateType() );
+                        detail::array_assign< DimensionCount, 2 >::apply( *this, CoordinateType() );
+                    }
 
-					/// @brief Constructor to set one value
-					explicit inline point( CoordinateType const& v0 )
-					: m_values( DimensionCount )
-				{
-					detail::array_assign< DimensionCount, 0 >::apply( m_values, v0 );
-					detail::array_assign< DimensionCount, 1 >::apply( m_values, CoordinateType() );
-					detail::array_assign< DimensionCount, 2 >::apply( m_values, CoordinateType() );
-				}
+                    /// @brief Constructor to set two values
+                    inline point( CoordinateType const& v0, CoordinateType const& v1 )
+                    : boost::numeric::ublas::vector< CoordinateType, ArrayType >( DimensionCount )
+                    {
+                        detail::array_assign< DimensionCount, 0 >::apply( *this, v0 );
+                        detail::array_assign< DimensionCount, 1 >::apply( *this, v1 );
+                        detail::array_assign< DimensionCount, 2 >::apply( *this, CoordinateType() );
+                    }
 
-					/// @brief Constructor to set two values
-					inline point( CoordinateType const& v0, CoordinateType const& v1 )
-					: m_values( DimensionCount )
-				{
-					detail::array_assign< DimensionCount, 0 >::apply( m_values, v0 );
-					detail::array_assign< DimensionCount, 1 >::apply( m_values, v1 );
-					detail::array_assign< DimensionCount, 2 >::apply( m_values, CoordinateType() );
-				}
+                    /// @brief Constructor to set three values
+                    inline point( CoordinateType const& v0, CoordinateType const& v1, CoordinateType const& v2 )
+                    : boost::numeric::ublas::vector< CoordinateType, ArrayType >( DimensionCount )
+                    {
+                        detail::array_assign< DimensionCount, 0 >::apply( *this, v0 );
+                        detail::array_assign< DimensionCount, 1 >::apply( *this, v1 );
+                        detail::array_assign< DimensionCount, 2 >::apply( *this, v2 );
+                    }
 
-					/// @brief Constructor to set three values
-					inline point( CoordinateType const& v0, CoordinateType const& v1, CoordinateType const& v2 )
-					: m_values( DimensionCount )
-				{
-					detail::array_assign< DimensionCount, 0 >::apply( m_values, v0 );
-					detail::array_assign< DimensionCount, 1 >::apply( m_values, v1 );
-					detail::array_assign< DimensionCount, 2 >::apply( m_values, v2 );
-				}
+                    /// @brief Copy constructor
+                    inline point( const point& p )
+                    // cause error C4996 in Microsoft Visual C++
+                    // : m_values( p.m_values )
+                    {
+                        // this is fine
+                        *this = p;
+                    }
 
-					/// @brief Copy constructor
-					inline point( const point& p )
-					// cause error C4996 in Microsoft Visual C++
-					// : m_values( p.m_values )
-					{
-						// this is fine
-						m_values = p.m_values;
-					}
+                    /// @brieft \~english  Get a coordinate
+                    ///         \~japanese 座標を取得する
+                    /// @tparam Index \~english  coordinate to get
+                    ///               \~japanese 取得する座標
+                    /// @return \~english  the coordinate
+                    ///         \~japanese 座標
+                    template< unsigned int Index >
+                    inline CoordinateType const& get() const
+                    {
+                        static_assert( Index < DimensionCount, "Index < DimensionCount must be true." );
+                        return operator()( Index );
+                    }
 
-					/// @brieft \~english  Get a coordinate
-					///         \~japanese 座標を取得する
-					/// @tparam Index \~english  coordinate to get
-					///               \~japanese 取得する座標
-					/// @return \~english  the coordinate
-					///         \~japanese 座標
-					template< unsigned int Index >
-					inline CoordinateType const& get() const
-					{
-						static_assert( Index < DimensionCount, "Index < DimensionCount must be true." );
-						return m_values( Index );
-					}
+                    /// @brief  \~english  Get vector implemented by Boost.uBLAS.
+                    ///         \~japanese Boost.uBLASによって実装されたベクターを取得する
+                    /// @return \~english  the vector of coordinates
+                    ///         \~japanese 座標ベクトル
+                    /// @{
+                    inline boost::numeric::ublas::vector< CoordinateType, ArrayType > const& get() const
+                    {
+                        return m_values;
+                    }
+                    inline boost::numeric::ublas::vector< CoordinateType, ArrayType >&       get()
+                    {
+                        return m_values;
+                    }
+                    /// @}
 
-					/// @brief  \~english  Get vector implemented by Boost.uBLAS.
-					///         \~japanese Boost.uBLASによって実装されたベクターを取得する
-					/// @return \~english  the vector of coordinates
-					///         \~japanese 座標ベクトル
-					/// @{
-					inline boost::numeric::ublas::vector< CoordinateType, ArrayType > const& get() const
-					{
-						return m_values;
-					}
-					inline boost::numeric::ublas::vector< CoordinateType, ArrayType >&       get()
-					{
-						return m_values;
-					}
-					/// @}
+                    /// @brief        \~english  Set a coordinate
+                    ///               \~japanese 座標を設定
+                    /// @tparam Index \~english  coordinate to set
+                    ///               \~japanese 設定する座標
+                    /// @param value  \~english  value to set
+                    ///               \~japanese セットする値
+                    template< std::size_t Index >
+                    void set( CoordinateType const& value )
+                    {
+                        static_assert( Index < DimensionCount, "Index < DimensionCount must be true." );
+                        operator()( Index ) = value;
+                    }
 
-					/// @brief        \~english  Set a coordinate
-					///               \~japanese 座標を設定
-					/// @tparam Index \~english  coordinate to set
-					///               \~japanese 設定する座標
-					/// @param value  \~english  value to set
-					///               \~japanese セットする値
-					template< std::size_t Index >
-					void set( CoordinateType const& value )
-					{
-						static_assert( Index < DimensionCount, "Index < DimensionCount must be true." );
-						m_values( Index ) = value;
-					}
-
-					/// @brief        \~english  Set coordinates vector
-					///               \~japanese 座標ベクトルを設定
-					/// @param values \~english  coordinates vector to set
-					///               \~japanese 設定する座標ベクトル
-					void set( CoordinateType const& values )
-					{
-						m_values = values;
-					}
-				};
-			}
-			using point_hide::point;
-		}
-	}
+                    /// @brief        \~english  Set coordinates vector
+                    ///               \~japanese 座標ベクトルを設定
+                    /// @param values \~english  coordinates vector to set
+                    ///               \~japanese 設定する座標ベクトル
+                    void set( CoordinateType const& values )
+                    {
+                        m_values = values;
+                    }
+                };
+            }
+            using point_hide::point;
+        }
+    }
 }
 
 // Adapt to Boost.Geometry
