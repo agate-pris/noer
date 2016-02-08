f4ecbaf87540a1238db53dda944fd5e12428fa4d
agate-pris <agate-pris@users.noreply.github.com>
2016-02-08 20:13:30 +0900

    Add copy and move constructor for cached_point

--- include/agate_pris/noer/collision_detection/cached_point.hpp
+++ include/agate_pris/noer/collision_detection/cached_point.hpp
@@ -12,6 +12,10 @@ namespace agate_pris
             private:
             Cache m_cache;
             Entity m_entity;
+
+            // copy and move constructor
+            cached_point( cached_point const& ) = default;
+            cached_point( cached_point&& ) = default;
         };
     }
 }
