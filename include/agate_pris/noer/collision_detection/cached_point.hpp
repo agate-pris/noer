f0d2f01e946036293467dffb5e85bdd66cbb0615
agate-pris <agate-pris@users.noreply.github.com>
2016-02-08 20:11:34 +0900

    Add class cached_point for hold cache of transformed point

--- include/agate_pris/noer/collision_detection/cached_point.hpp
+++ include/agate_pris/noer/collision_detection/cached_point.hpp
@@ -1,4 +1,16 @@
 
 #ifndef AGATE_PRIS_NOER_COLLISION_DETECTION_CACHED_POINT_HPP
 #define AGATE_PRIS_NOER_COLLISION_DETECTION_CACHED_POINT_HPP
+
+namespace agate_pris
+{
+    namespace noer
+    {
+        template< typename Cache, typename Entity >
+        class cached_point
+        {
+        };
+    }
+}
+
 #endif
