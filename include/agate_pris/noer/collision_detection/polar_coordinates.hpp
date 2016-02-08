94f2417a684d55b4a8736993f9ff7e2f6d0274f2
agate-pris <agate-pris@users.noreply.github.com>
2016-02-08 23:49:03 +0900

    Add polar coordinates class

--- include/agate_pris/noer/collision_detection/polar_coordinates.hpp
+++ include/agate_pris/noer/collision_detection/polar_coordinates.hpp
@@ -1,4 +1,22 @@
 
 #ifndef AGATE_PRIS_NOER_COLLISION_DETECTION_POLAR_COORDINATES_HPP
 #define AGATE_PRIS_NOER_COLLISION_DETECTION_POLAR_COORDINATES_HPP
+
+namespace agate_pris
+{
+    namespace noer
+    {
+        namespace collision_detection
+        {
+            template< typename RadiusType, typename AngleType >
+            class polar_coordinates
+            {
+                private:
+                RadiusType m_radius;
+                AngleType  m_angle;
+            };
+        }
+    }
+}
+
 #endif
