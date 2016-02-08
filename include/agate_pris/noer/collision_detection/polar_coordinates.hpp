c2b44b8243099e05ffbcec823bbcf1a576b40e42
agate-pris <agate-pris@users.noreply.github.com>
2016-02-08 23:54:42 +0900

    Fix inclusion of polar_coordinates.hpp

--- include/agate_pris/noer/collision_detection/polar_coordinates.hpp
+++ include/agate_pris/noer/collision_detection/polar_coordinates.hpp
@@ -2,6 +2,8 @@
 #ifndef AGATE_PRIS_NOER_COLLISION_DETECTION_POLAR_COORDINATES_HPP
 #define AGATE_PRIS_NOER_COLLISION_DETECTION_POLAR_COORDINATES_HPP
 
+#include <utility>
+
 namespace agate_pris
 {
     namespace noer
