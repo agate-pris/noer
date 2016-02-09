cc123345aae02333e5742b25b3c5c48d31a4d162
agate-pris <agate-pris@users.noreply.github.com>
2016-02-09 19:44:59 +0900

    Add class template spherical_coordinates

--- include/agate_pris/noer/collision_detection/spherical_coordinates.hpp
+++ include/agate_pris/noer/collision_detection/spherical_coordinates.hpp
@@ -1,4 +1,24 @@
 
 #ifndef AGATE_PRIS_NOER_COLLISION_DETECTION_SPHERICAL_COORDINATES_HPP
 #define AGATE_PRIS_NOER_COLLISION_DETECTION_SPHERICAL_COORDINATES_HPP
+
+namespace agate_pris
+{
+    namespace noer
+    {
+        namespace collision_detection
+        {
+            // 1st angle behave as polar angle from x-axis
+            // 2nd angle behave as azimuthal angle of orthogonal projection on yz-plane from y-axis
+            template< typename RadiusType, typename AnglesType >
+            class spherical_coordinates
+            {
+                private:
+                RadiusType m_radius;
+                AnglesType m_angles;
+            };
+        }
+    }
+}
+
 #endif
