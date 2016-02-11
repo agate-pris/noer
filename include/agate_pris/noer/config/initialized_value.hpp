a37e25b6511537025e925ae98a07e7c9fbdc2504
agate-pris <agate-pris@users.noreply.github.com>
2016-02-11 13:02:45 +0900

    Add class initialized_value

--- include/agate_pris/noer/config/initialized_value.hpp
+++ include/agate_pris/noer/config/initialized_value.hpp
@@ -1,4 +1,20 @@
 
 #ifndef AGATE_PRIS_NOER_INITIALIZED_VALUE_HPP
 #define AGATE_PRIS_NOER_INITIALIZED_VALUE_HPP
+
+namespace agate_pris
+{
+    namespace noer
+    {
+        namespace initialized_values
+        {
+            template< typename Value, typename Tag = void >
+            class initialized_value
+            {
+            };
+        }
+        using initialized_values::initialized_value;
+    }
+}
+
 #endif
