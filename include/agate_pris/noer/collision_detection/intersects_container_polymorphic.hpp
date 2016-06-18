2dd667ba761c025cf2b739eff77a91d9f5e3d4c9
agate-pris <agate-pris@users.noreply.github.com>
2016-06-19 00:31:42 +0900

    Adjust indent and space in intersects_container_polymorphic.hpp

--- include/agate_pris/noer/collision_detection/intersects_container_polymorphic.hpp
+++ include/agate_pris/noer/collision_detection/intersects_container_polymorphic.hpp
@@ -1,4 +1,4 @@
-
+﻿
 #ifndef AGATE_PRIS_NOER_COLLISION_DETECTION_INTERSECTS_CONTAINER_POLYMORPHIC_HPP
 #define AGATE_PRIS_NOER_COLLISION_DETECTION_INTERSECTS_CONTAINER_POLYMORPHIC_HPP
 
@@ -9,37 +9,36 @@
 #include <boost/fusion/adapted/std_tuple.hpp>
 #include <boost/fusion/include/std_tuple.hpp>
 
-namespace agate_pris
-{
-    namespace noer
-    {
-        namespace collision_detection
-        {
-            template< typename CollisionDetection1, typename CollisionDetection2 >
-            inline bool intersects( CollisionDetection1 const& p, CollisionDetection2 const& c, polymorphic_tag, container_tag )
-            {
-                // e is element of c
-                auto f = [ &p ]( auto& e )
-                {
-                    return intersects( p, e );
-                };
-                
-                return boost::fusion::any( c, f );
-            }
+namespace agate_pris {
+namespace noer {
+namespace collision_detection {
 
-            template< typename CollisionDetection1, typename CollisionDetection2 >
-            inline bool intersects( CollisionDetection1 const& c, CollisionDetection2 const& p, container_tag, polymorphic_tag )
-            {
-                // e is element of c
-                auto f = [ &p ]( auto& e )
-                {
-                    return intersects( p, e );
-                };
-                
-                return boost::fusion::any( c, f );
-            }
-        }
-    }
+template< typename CollisionDetection1, typename CollisionDetection2 >
+inline bool intersects( CollisionDetection1 const& p, CollisionDetection2 const& c, polymorphic_tag, container_tag )
+{
+	// e is element of c
+	auto f = [ &p ]( auto& e )
+	{
+		return intersects( p, e );
+	};
+
+	return boost::fusion::any( c, f );
 }
+
+template< typename CollisionDetection1, typename CollisionDetection2 >
+inline bool intersects( CollisionDetection1 const& c, CollisionDetection2 const& p, container_tag, polymorphic_tag )
+{
+	// e is element of c
+	auto f = [ &p ]( auto& e )
+	{
+		return intersects( p, e );
+	};
+
+	return boost::fusion::any( c, f );
+}
+
+} // collision_detection
+} // noer
+} // agate_pris
 
 #endif
