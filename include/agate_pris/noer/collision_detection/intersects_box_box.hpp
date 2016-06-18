d3f1e5107cff58f49b9b2b5463ec25ca133ecca2
agate-pris <agate-pris@users.noreply.github.com>
2016-06-18 23:47:34 +0900

    Adjust indent and space in intersects_box_box.hpp

--- include/agate_pris/noer/collision_detection/intersects_box_box.hpp
+++ include/agate_pris/noer/collision_detection/intersects_box_box.hpp
@@ -1,23 +1,22 @@
-
+﻿
 #ifndef AGATE_PRIS_NOER_COLLISION_DETECTION_INTERSECTS_BOX_BOX_HPP
 #define AGATE_PRIS_NOER_COLLISION_DETECTION_INTERSECTS_BOX_BOX_HPP
 
 #include <agate_pris/noer/collision_detection/tags.hpp>
 #include <boost/geometry/algorithms/intersects.hpp>
 
-namespace agate_pris
+namespace agate_pris {
+namespace noer {
+namespace collision_detection {
+
+template< typename BoxType >
+inline bool intersects( BoxType const& a, BoxType const& b, box_tag, box_tag )
 {
-	namespace noer
-	{
-		namespace collision_detection
-		{
-			template< typename BoxType >
-			inline bool intersects( BoxType const& a, BoxType const& b, box_tag, box_tag )
-			{
-				return boost::geometry::intersects( a, b );
-			}
-		}
-	}
+	return boost::geometry::intersects( a, b );
 }
+
+} // collision_detection
+} // noer
+} // agate_pris
 
 #endif
