7668e149a5813544292dbce26adf3ff7e3f4fd12
agate-pris <agate-pris@users.noreply.github.com>
2016-06-19 00:06:42 +0900

    Adjust indent and space in intersects_box_segment.hpp

--- include/agate_pris/noer/collision_detection/intersects_box_segment.hpp
+++ include/agate_pris/noer/collision_detection/intersects_box_segment.hpp
@@ -1,29 +1,28 @@
-
+﻿
 #ifndef AGATE_PRIS_NOER_COLLISION_DETECTION_INTERSECTS_BOX_SEGMENT_HPP
 #define AGATE_PRIS_NOER_COLLISION_DETECTION_INTERSECTS_BOX_SEGMENT_HPP
 
 #include <agate_pris/noer/collision_detection/tags.hpp>
 #include <boost/geometry/algorithms/intersects.hpp>
 
-namespace agate_pris
-{
-	namespace noer
-	{
-		namespace collision_detection
-		{
-			template< typename BoxType, typename SegmentType >
-			inline bool intersects( BoxType const& b, SegmentType const& s, box_tag, segment_tag )
-			{
-				return boost::geometry::intersects( b, s );
-			}
+namespace agate_pris {
+namespace noer {
+namespace collision_detection {
 
-			template< typename BoxType, typename SegmentType >
-			inline bool intersects( SegmentType const& s, BoxType const& b, segment_tag, box_tag )
-			{
-				return boost::geometry::intersects( s, b );
-			}
-		}
-	}
+template< typename BoxType, typename SegmentType >
+inline bool intersects( BoxType const& b, SegmentType const& s, box_tag, segment_tag )
+{
+	return boost::geometry::intersects( b, s );
 }
+
+template< typename BoxType, typename SegmentType >
+inline bool intersects( SegmentType const& s, BoxType const& b, segment_tag, box_tag )
+{
+	return boost::geometry::intersects( s, b );
+}
+
+} // collision_detection
+} // noer
+} // agate_pris
 
 #endif
