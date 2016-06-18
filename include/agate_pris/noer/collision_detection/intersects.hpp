5b80d0d9e93a75d34ebacd6e73ed8dcd52ce2b2d
agate-pris <agate-pris@users.noreply.github.com>
2016-06-18 23:44:44 +0900

    Adjust indent and space in intersects.hpp

--- include/agate_pris/noer/collision_detection/intersects.hpp
+++ include/agate_pris/noer/collision_detection/intersects.hpp
@@ -1,4 +1,4 @@
-
+﻿
 #ifndef AGATE_PRIS_NOER_COLLISION_DETECTION_INTERSECTS_HPP
 #define AGATE_PRIS_NOER_COLLISION_DETECTION_INTERSECTS_HPP
 
@@ -14,19 +14,18 @@
 #include <agate_pris/noer/collision_detection/intersects_segment_triangle.hpp>
 #include <agate_pris/noer/collision_detection/intersects_triangle_triangle.hpp>
 
-namespace agate_pris
+namespace agate_pris {
+namespace noer {
+namespace collision_detection {
+
+template< typename CollisionDetection1, typename CollisionDetection2 >
+inline bool intersects( CollisionDetection1 const& a, CollisionDetection2 const& b )
 {
-	namespace noer
-	{
-		namespace collision_detection
-		{
-			template< typename CollisionDetection1, typename CollisionDetection2 >
-			inline bool intersects( CollisionDetection1 const& a, CollisionDetection2 const& b )
-			{
-				return intersects( a, b, tag_t< CollisionDetection1 >{}, tag_t< CollisionDetection2 >{} );
-			}
-		}
-	}
+	return intersects( a, b, tag_t< CollisionDetection1 >{}, tag_t< CollisionDetection2 >{} );
 }
+
+} // collision_detection
+} // noer
+} // agate_pris
 
 #endif
