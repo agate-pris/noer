3a53d0cdca488ed1a4b2b8dbee03d5ce2c9f65a3
agate-pris <agate-pris@users.noreply.github.com>
2016-06-26 20:34:49 +0900

    Adjust indent and space in polymorphic_interface.hpp

--- include/agate_pris/noer/collision_detection/polymorphic_interface.hpp
+++ include/agate_pris/noer/collision_detection/polymorphic_interface.hpp
@@ -7,86 +7,85 @@
 #include <agate_pris/noer/collision_detection/tags.hpp>
 #include <type_traits>
 
-namespace agate_pris
+namespace agate_pris {
+namespace noer {
+namespace collision_detection {
+
+namespace detail {
+
+template< typename... Objects >
+struct polymorphic_interface {};
+
+template< typename Last >
+struct polymorphic_interface< Last >
 {
-    namespace noer
-    {
-        namespace collision_detection
-        {
-            namespace detail
-            {
-                template< typename... Objects >
-                struct polymorphic_interface {};
+	virtual bool intersects( Last const& object ) const = 0;
+};
 
-                template< typename Last >
-                struct polymorphic_interface< Last >
-                {
-                    virtual bool intersects( Last const& object ) const = 0;
-                };
+template< typename Head, typename... Tail >
+struct polymorphic_interface< Head, Tail... >
+	: polymorphic_interface< Tail... >
+{
+	using polymorphic_interface< Tail... >::intersects;
+	virtual bool intersects( Head const& object ) const = 0;
+};
 
-                template< typename Head, typename... Tail >
-                struct polymorphic_interface< Head, Tail... >
-                : polymorphic_interface< Tail... >
-                {
-                    using polymorphic_interface< Tail... >::intersects;
-                    virtual bool intersects( Head const& object ) const = 0;
-                };
-            }
+} // detail
 
-            template< typename... Objects >
-            struct polymorphic_interface
-            : detail::polymorphic_interface< Objects... >
-            {
-                template< typename Subject >
-                using implement_type = polymorphic_implement< Subject, Objects... >;
-                using interface_type = polymorphic_interface< Objects... >;
-                using detail::polymorphic_interface< Objects... >::intersects;
-                virtual bool intersects( interface_type const& ) const = 0;
-            };
+template< typename... Objects >
+struct polymorphic_interface
+	: detail::polymorphic_interface< Objects... >
+{
+	template< typename Subject >
+	using implement_type = polymorphic_implement< Subject, Objects... >;
+	using interface_type = polymorphic_interface< Objects... >;
+	using detail::polymorphic_interface< Objects... >::intersects;
+	virtual bool intersects( interface_type const& ) const = 0;
+};
 
-            template<>
-            struct polymorphic_interface<>
-            {
-                template< typename Subject >
-                using implement_type = polymorphic_implement< Subject >;
-                using interface_type = polymorphic_interface<>;
-                virtual bool intersects( interface_type const& ) const = 0;
-            };
+template<>
+struct polymorphic_interface<>
+{
+	template< typename Subject >
+	using implement_type = polymorphic_implement< Subject >;
+	using interface_type = polymorphic_interface<>;
+	virtual bool intersects( interface_type const& ) const = 0;
+};
 
-            namespace traits
-            {
-                template
-                <
-                    typename... Objects
-                >
-                struct tag< polymorphic_interface< Objects... > >
-                {
-                    using type = polymorphic_tag;
-                };
-            }
-
-            // overload intersects
-            template< typename... Objects >
-            inline bool intersects( polymorphic_interface< Objects... > const& a, polymorphic_interface< Objects... > const& b )
-            {
-                return a.intersects( b );
-            }
-
-            template< typename AnyObject, typename AnyTag, typename... Objects >
-            inline auto intersects( polymorphic_interface< Objects... > const& a, AnyObject const& b, polymorphic_tag, AnyTag )
-            -> std::enable_if_t< !std::is_same< AnyTag, container_tag >::value, bool >
-            {
-                return a.intersects( b );
-            }
-
-            template< typename AnyObject, typename AnyTag, typename... Objects >
-            inline auto intersects( AnyObject const& b, polymorphic_interface< Objects... > const& a, AnyTag, polymorphic_tag )
-            -> std::enable_if_t< !std::is_same< AnyTag, container_tag >::value, bool >
-            {
-                return a.intersects( b );
-            }
-        }
-    }
+namespace traits {
+template
+<
+	typename... Objects
+>
+struct tag< polymorphic_interface< Objects... > >
+{
+	using type = polymorphic_tag;
+};
 }
+
+// overload intersects
+template< typename... Objects >
+inline bool intersects( polymorphic_interface< Objects... > const& a, polymorphic_interface< Objects... > const& b )
+{
+	return a.intersects( b );
+}
+
+template< typename AnyObject, typename AnyTag, typename... Objects >
+inline auto intersects( polymorphic_interface< Objects... > const& a, AnyObject const& b, polymorphic_tag, AnyTag )
+-> std::enable_if_t< !std::is_same< AnyTag, container_tag >::value, bool >
+{
+	return a.intersects( b );
+}
+
+template< typename AnyObject, typename AnyTag, typename... Objects >
+inline auto intersects( AnyObject const& b, polymorphic_interface< Objects... > const& a, AnyTag, polymorphic_tag )
+-> std::enable_if_t< !std::is_same< AnyTag, container_tag >::value, bool >
+{
+	return a.intersects( b );
+}
+
+} // collision_detection
+} // noer
+} // agate_pris
 
 #endif
