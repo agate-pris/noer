edbb2b92848faed74eaf7d0fa50eda879bab5f00
agate-pris <agate-pris@users.noreply.github.com>
2016-06-26 20:30:56 +0900

    Adjust indent and space in polymorphic_implement_fwd.hpp

--- include/agate_pris/noer/collision_detection/polymorphic_implement_fwd.hpp
+++ include/agate_pris/noer/collision_detection/polymorphic_implement_fwd.hpp
@@ -1,17 +1,16 @@
-
+﻿
 #ifndef AGATE_PRIS_NOER_COLLISION_DETECTION_POLYMORPHIC_IMPLEMENT_FWD_HPP
 #define AGATE_PRIS_NOER_COLLISION_DETECTION_POLYMORPHIC_IMPLEMENT_FWD_HPP
 
-namespace agate_pris
-{
-    namespace noer
-    {
-        namespace collision_detection
-        {
-            template< typename Subject, typename... Objects >
-            struct polymorphic_implement;
-        }
-    }
-}
+namespace agate_pris {
+namespace noer {
+namespace collision_detection {
+
+template< typename Subject, typename... Objects >
+struct polymorphic_implement;
+
+} // collision_detection
+} // noer
+} // agate_pris
 
 #endif
