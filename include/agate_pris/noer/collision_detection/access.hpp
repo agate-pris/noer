d536205a6ed66deaa9fdfa45c6dead773abfae27
agate-pris <agate-pris@users.noreply.github.com>
2016-07-03 12:59:59 +0900

    Adjust indent and space.
    collision_detection/access.hpp

--- include/agate_pris/noer/collision_detection/access.hpp
+++ include/agate_pris/noer/collision_detection/access.hpp
@@ -6,44 +6,44 @@
 #include <boost/geometry/core/coordinate_type.hpp>
 #include <boost/geometry/core/access.hpp>
 
-namespace agate_pris
+namespace agate_pris {
+namespace noer {
+namespace collision_detection {
+
+namespace traits {
+
+template< typename CollisionDetection, std::size_t Index, std::size_t Dimension >
+struct indexed_access
 {
-	namespace noer
-	{
-		namespace collision_detection
-		{
-			namespace traits
-			{
-				template< typename CollisionDetection, std::size_t Index, std::size_t Dimension >
-				struct indexed_access
-				{
-					using coordinate_type = typename boost::geometry::coordinate_type< CollisionDetection >::type;
+    using coordinate_type = typename boost::geometry::coordinate_type< CollisionDetection >::type;
 
-					static inline coordinate_type get( CollisionDetection const& cd )
-					{
-						return boost::geometry::get< Index, Dimension >( cd );
-					}
+    static inline coordinate_type get( CollisionDetection const& cd )
+    {
+        return boost::geometry::get< Index, Dimension >( cd );
+    }
 
-					static inline void set( CollisionDetection& cd, coordinate_type const& value )
-					{
-						boost::geometry::set< Index, Dimension >( cd, value );
-					}
-				};
-			}
-			
-			template< std::size_t Index, std::size_t Dimension, typename CollisionDetectionType >
-			auto get( const CollisionDetectionType& cd )
-			{
-				return traits::indexed_access< CollisionDetectionType, Index, Dimension >::get( cd );
-			}
+    static inline void set( CollisionDetection& cd, coordinate_type const& value )
+    {
+        boost::geometry::set< Index, Dimension >( cd, value );
+    }
+};
 
-			template< std::size_t Index, std::size_t Dimension, typename CollisionDetectionType, typename CoordinateType >
-			void set( CollisionDetectionType& cd, const CoordinateType& value )
-			{
-				traits::indexed_access< CollisionDetectionType, Index, Dimension >::set( cd, value );
-			}
-		}
-	}
+} // traits
+
+template< std::size_t Index, std::size_t Dimension, typename CollisionDetectionType >
+auto get( const CollisionDetectionType& cd )
+{
+    return traits::indexed_access< CollisionDetectionType, Index, Dimension >::get( cd );
 }
+
+template< std::size_t Index, std::size_t Dimension, typename CollisionDetectionType, typename CoordinateType >
+void set( CollisionDetectionType& cd, const CoordinateType& value )
+{
+    traits::indexed_access< CollisionDetectionType, Index, Dimension >::set( cd, value );
+}
+
+} // collision_detection
+} // noer
+} // agate_pris
 
 #endif
