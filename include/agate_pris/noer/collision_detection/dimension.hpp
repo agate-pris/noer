ffdeed494d5d921a9ad27e42352ef969eb1edeb7
agate-pris <agate-pris@users.noreply.github.com>
2016-07-03 13:35:22 +0900

    Adjust indent and space.
    collision_detection/dimension.hpp

--- include/agate_pris/noer/collision_detection/dimension.hpp
+++ include/agate_pris/noer/collision_detection/dimension.hpp
@@ -6,20 +6,19 @@
 #include <agate_pris/noer/collision_detection/point_type.hpp>
 #include <boost/geometry/core/coordinate_dimension.hpp>
 
-namespace agate_pris
+namespace agate_pris {
+namespace noer {
+namespace collision_detection {
+
+template< typename CollisionDetectionType >
+struct dimension
 {
-	namespace noer
-	{
-		namespace collision_detection
-		{
-			template< typename CollisionDetectionType >
-			struct dimension
-			{
-				static constexpr const std::size_t value =
-				boost::geometry::dimension< point_type_t< CollisionDetectionType > >::value;
-			};
-		}
-	}
-}
+    static constexpr const std::size_t value =
+    boost::geometry::dimension< point_type_t< CollisionDetectionType > >::value;
+};
+
+} // collision_detection
+} // noer
+} // agate_pris
 
 #endif
