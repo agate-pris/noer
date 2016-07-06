c3b6e1f0675e3fa21e72d0626fc8948bfe059932
agate-pris <agate-pris@users.noreply.github.com>
2016-07-06 23:16:06 +0900

    rename class shared_point as point_shared

--- include/agate_pris/noer/collision_detection/shared_point.hpp
+++ include/agate_pris/noer/collision_detection/shared_point.hpp
@@ -18,13 +18,13 @@ namespace collision_detection {
 /// @brief \~english class for share point using  `std::shared_ptr`
 /// \~japanese `std::shared_ptr` によって座標を共有するクラス
 template< typename PointType >
-struct shared_point
+struct point_shared
 {
     std::shared_ptr< PointType > ptr;
 
     /// @brief \~english default constructor
     /// \~japanese デフォルトコンストラクタ
-    constexpr explicit shared_point() noexcept {};
+    constexpr explicit point_shared() noexcept {};
 
     /// @brief \~english constructor for initialize `shared_ptr`
     /// \~japanese shared_ptr を初期化するコンストラクタ
@@ -36,17 +36,17 @@ struct shared_point
         <
             !std::is_base_of
             <
-                shared_point< PointType >,
+                point_shared< PointType >,
                 std::decay_t< Arg >
             >::value
         >
     >
-    shared_point( Arg&& arg )
+    point_shared( Arg&& arg )
     : ptr( std::forward< Arg >( arg ) )
     {}
 
     template< typename First, typename Second, typename... Tail >
-    inline shared_point( First&& first, Second&& second, Tail&&... tail )
+    inline point_shared( First&& first, Second&& second, Tail&&... tail )
     : ptr
     (
         std::forward< First >( first ),
@@ -66,33 +66,33 @@ namespace geometry {
 namespace traits {
 
 template< typename PointType >
-struct tag< agate_pris::noer::collision_detection::shared_point< PointType > >
+struct tag< agate_pris::noer::collision_detection::point_shared< PointType > >
 {
     using type = point_tag;
 };
 
 template< typename PointType >
-struct coordinate_type< agate_pris::noer::collision_detection::shared_point< PointType > >
+struct coordinate_type< agate_pris::noer::collision_detection::point_shared< PointType > >
 {
     using type = typename coordinate_type< PointType >::type;
 };
 
 template< typename PointType >
-struct coordinate_system< agate_pris::noer::collision_detection::shared_point< PointType > >
+struct coordinate_system< agate_pris::noer::collision_detection::point_shared< PointType > >
 {
     using type = typename coordinate_system< PointType >::type;
 };
 
 template< typename PointType >
-struct dimension< agate_pris::noer::collision_detection::shared_point< PointType > >
+struct dimension< agate_pris::noer::collision_detection::point_shared< PointType > >
 {
     static constexpr const std::size_t value = dimension< PointType >::value;
 };
 
 template< typename PointType, std::size_t Dimension >
-struct access< agate_pris::noer::collision_detection::shared_point< PointType >, Dimension >
+struct access< agate_pris::noer::collision_detection::point_shared< PointType >, Dimension >
 {
-    static inline decltype( auto ) get( agate_pris::noer::collision_detection::shared_point< PointType > const& p )
+    static inline decltype( auto ) get( agate_pris::noer::collision_detection::point_shared< PointType > const& p )
     {
         if( !p.ptr ) throw;
         return geometry::get< Dimension >( *( p.ptr ) );
@@ -100,7 +100,7 @@ struct access< agate_pris::noer::collision_detection::shared_point< PointType >,
 
     static inline void set
     (
-        agate_pris::noer::collision_detection::shared_point< PointType >& p,
+        agate_pris::noer::collision_detection::point_shared< PointType >& p,
         typename coordinate_type< PointType >::type const& value
     )
     {
