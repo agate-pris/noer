e5db01e7b223bc43eb0fe2464e96e0d15afa98df
agate-pris <agate-pris@users.noreply.github.com>
2016-07-08 20:43:16 +0900

    Rename class collision_detection::point_shared to collision_detection::point::shared::entity

--- include/agate_pris/noer/collision_detection/point_shared.hpp
+++ include/agate_pris/noer/collision_detection/point_shared.hpp
@@ -16,9 +16,11 @@
 namespace agate_pris {
 namespace noer {
 namespace collision_detection {
+namespace point {
+namespace shared {
 
 template< typename Point >
-class point_shared
+class entity
 {
     // type
     using element_type   = Point;
@@ -31,19 +33,19 @@ class point_shared
     // constructor
     public:
     // construct empty shared_ptr
-    BOOST_CONSTEXPR point_shared() BOOST_NOEXCEPT_OR_NOTHROW;
+    BOOST_CONSTEXPR entity() BOOST_NOEXCEPT_OR_NOTHROW;
     // construct empty shared_ptr
-    BOOST_CONSTEXPR point_shared( std::nullptr_t ) BOOST_NOEXCEPT_OR_NOTHROW;
+    BOOST_CONSTEXPR entity( std::nullptr_t ) BOOST_NOEXCEPT_OR_NOTHROW;
 
     // construct shared_ptr object that owns same resource as r
-    point_shared( point_shared< Point > const& r ) BOOST_NOEXCEPT_OR_NOTHROW;
+    entity( entity< Point > const& r ) BOOST_NOEXCEPT_OR_NOTHROW;
     // construct shared_ptr object that takes resource from r
-    point_shared( point_shared< Point >&& r ) BOOST_NOEXCEPT_OR_NOTHROW;
+    entity( entity< Point >&& r ) BOOST_NOEXCEPT_OR_NOTHROW;
 
     // construct shared_ptr object that owns same resource as r
-    point_shared( shared_pointer const& r ) BOOST_NOEXCEPT_OR_NOTHROW;
+    entity( shared_pointer const& r ) BOOST_NOEXCEPT_OR_NOTHROW;
     // construct shared_ptr object that takes resource from r
-    point_shared( shared_pointer&& r ) BOOST_NOEXCEPT_OR_NOTHROW;
+    entity( shared_pointer&& r ) BOOST_NOEXCEPT_OR_NOTHROW;
 
     // public function
     public:
@@ -53,33 +55,33 @@ class point_shared
 
 // construct empty shared_ptr
 template< typename Point >
-inline point_shared< Point >::point_shared() BOOST_NOEXCEPT_OR_NOTHROW {}
+inline entity< Point >::entity() BOOST_NOEXCEPT_OR_NOTHROW {}
 
 // construct empty shared_ptr
 template< typename Point >
-inline point_shared< Point >::point_shared( std::nullptr_t ) BOOST_NOEXCEPT_OR_NOTHROW {}
+inline entity< Point >::entity( std::nullptr_t ) BOOST_NOEXCEPT_OR_NOTHROW {}
 
 // construct shared_ptr object that owns same resource as r
 template< typename Point >
-inline point_shared< Point >::point_shared( point_shared< Point > const& r ) BOOST_NOEXCEPT_OR_NOTHROW
+inline entity< Point >::entity( entity< Point > const& r ) BOOST_NOEXCEPT_OR_NOTHROW
     : m_ptr( r.data() )
 {}
 
 // construct shared_ptr object that takes resource from r
 template< typename Point >
-inline point_shared< Point >::point_shared( point_shared< Point >&& r ) BOOST_NOEXCEPT_OR_NOTHROW
+inline entity< Point >::entity( entity< Point >&& r ) BOOST_NOEXCEPT_OR_NOTHROW
     : m_ptr( std::move( r.data() ) )
 {}
 
 // construct shared_ptr object that owns same resource as r
 template< typename Point >
-inline point_shared< Point >::point_shared( shared_pointer const& r ) BOOST_NOEXCEPT_OR_NOTHROW
+inline entity< Point >::entity( shared_pointer const& r ) BOOST_NOEXCEPT_OR_NOTHROW
     : m_ptr( r )
 {}
 
 // construct shared_ptr object that takes resource from r
 template< typename Point >
-inline point_shared< Point >::point_shared( shared_pointer&& r ) BOOST_NOEXCEPT_OR_NOTHROW
+inline entity< Point >::entity( shared_pointer&& r ) BOOST_NOEXCEPT_OR_NOTHROW
     : m_ptr( std::move( r ) )
 {}
 
@@ -88,18 +90,20 @@ inline point_shared< Point >::point_shared( shared_pointer&& r ) BOOST_NOEXCEPT_
 
 // get shared_ptr const reference
 template< typename Point >
-inline typename point_shared< Point >::shared_pointer const& point_shared< Point >::data() const BOOST_NOEXCEPT_OR_NOTHROW
+inline typename entity< Point >::shared_pointer const& entity< Point >::data() const BOOST_NOEXCEPT_OR_NOTHROW
 {
     return m_ptr;
 }
 
 // get shared_ptr reference
 template< typename Point >
-inline typename point_shared< Point >::shared_pointer& point_shared< Point >::data() BOOST_NOEXCEPT_OR_NOTHROW
+inline typename entity< Point >::shared_pointer& entity< Point >::data() BOOST_NOEXCEPT_OR_NOTHROW
 {
     return m_ptr;
 }
 
+} // shared
+} // point
 } // collision_detection
 } // noer
 } // agate_pris
@@ -109,33 +113,33 @@ namespace geometry {
 namespace traits {
 
 template< typename Point >
-struct tag< agate_pris::noer::collision_detection::point_shared< Point > >
+struct tag< agate_pris::noer::collision_detection::point::shared::entity< Point > >
 {
     using type = point_tag;
 };
 
 template< typename Point >
-struct coordinate_type< agate_pris::noer::collision_detection::point_shared< Point > >
+struct coordinate_type< agate_pris::noer::collision_detection::point::shared::entity< Point > >
 {
     using type = typename coordinate_type< Point >::type;
 };
 
 template< typename Point >
-struct coordinate_system< agate_pris::noer::collision_detection::point_shared< Point > >
+struct coordinate_system< agate_pris::noer::collision_detection::point::shared::entity< Point > >
 {
     using type = typename coordinate_system< Point >::type;
 };
 
 template< typename Point >
-struct dimension< agate_pris::noer::collision_detection::point_shared< Point > >
+struct dimension< agate_pris::noer::collision_detection::point::shared::entity< Point > >
 {
     static constexpr const std::size_t value = dimension< Point >::value;
 };
 
 template< typename Point, std::size_t Dimension >
-struct access< agate_pris::noer::collision_detection::point_shared< Point >, Dimension >
+struct access< agate_pris::noer::collision_detection::point::shared::entity< Point >, Dimension >
 {
-    static inline decltype( auto ) get( agate_pris::noer::collision_detection::point_shared< Point > const& p )
+    static inline decltype( auto ) get( agate_pris::noer::collision_detection::point::shared::entity< Point > const& p )
     {
         if( !p.m_ptr ) throw;
         return geometry::get< Dimension >( *( p.m_ptr ) );
@@ -143,7 +147,7 @@ struct access< agate_pris::noer::collision_detection::point_shared< Point >, Dim
 
     static inline void set
     (
-        agate_pris::noer::collision_detection::point_shared< Point >& p,
+        agate_pris::noer::collision_detection::point::shared::entity< Point >& p,
         typename coordinate_type< Point >::type const& value
     )
     {
