6bc0d82a96d0f5891a203fc929f58f9ba75b11f6
agate-pris <agate-pris@users.noreply.github.com>
2016-07-09 15:26:57 +0900

    Add constructor to collision_detection::point::shared::check_zero to construct empty shared_ptr

--- include/agate_pris/noer/collision_detection/point/shared/check_zero.hpp
+++ include/agate_pris/noer/collision_detection/point/shared/check_zero.hpp
@@ -2,6 +2,8 @@
 #ifndef AGATE_PRIS_NOER_COLLISION_DETECTION_POINT_SHARED_CHECK_ZERO_HPP
 #define AGATE_PRIS_NOER_COLLISION_DETECTION_POINT_SHARED_CHECK_ZERO_HPP
 
+#include <cstddef>
+#include <boost/config.hpp>
 #include <agate_pris/noer/collision_detection/point/shared.hpp>
 
 namespace agate_pris {
@@ -14,7 +16,28 @@ template< typename Point >
 class check_zero
     : public entity< Point >
 {
+    // constuctor
+    public:
+    // construct empty shared_ptr
+    BOOST_CONSTEXPR check_zero() BOOST_NOEXCEPT_OR_NOTHROW;
+    // construct empty shared_ptr
+    BOOST_CONSTEXPR check_zero( std::nullptr_t ) BOOST_NOEXCEPT_OR_NOTHROW;
 };
+
+// constructor
+// ---------------------------------------------------------------------
+
+// construct empty shared_ptr
+template< typename Point >
+inline check_zero< Point >::check_zero() BOOST_NOEXCEPT_OR_NOTHROW
+    : entity< Point >()
+{}
+
+// construct empty shared_ptr
+template< typename Point >
+inline check_zero< Point >::check_zero( std::nullptr_t ) BOOST_NOEXCEPT_OR_NOTHROW
+    : entity< Point >()
+{}
 
 } // shared
 } // point
