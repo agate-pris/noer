ff3b50feb709688e9e2367a43cea3f0f9dbe6af6
agate-pris <agate-pris@users.noreply.github.com>
2016-07-09 19:09:40 +0900

    Add constructor for shared_pointer&& to collision_detection::point::shared::check_zero

--- include/agate_pris/noer/collision_detection/point/shared/check_zero.hpp
+++ include/agate_pris/noer/collision_detection/point/shared/check_zero.hpp
@@ -31,6 +31,9 @@ class check_zero
 
     // construct shared_ptr object that owns same resource as r
     check_zero( shared_pointer const& r ) BOOST_NOEXCEPT_OR_NOTHROW;
+
+    // construct shared_ptr object that takes resource from r
+    check_zero( shared_pointer&& r ) BOOST_NOEXCEPT_OR_NOTHROW;
 };
 
 // constructor
@@ -64,6 +67,12 @@ inline check_zero< Point >::check_zero( check_zero< Point >&& r ) BOOST_NOEXCEPT
 template< typename Point >
 inline check_zero< Point >::check_zero( shared_pointer const& r ) BOOST_NOEXCEPT_OR_NOTHROW
     : entity< Point >( r )
+{}
+
+// construct shared_ptr object that takes resource from r
+template< typename Point >
+inline check_zero< Point >::check_zero( shared_pointer&& r ) BOOST_NOEXCEPT_OR_NOTHROW
+    : entity< Point >( std::move( r ) )
 {}
 
 } // shared
