c93b8b22b6878625e5ce983eef62ba031b1f661e
agate-pris <agate-pris@users.noreply.github.com>
2016-07-11 01:11:07 +0900

    Add class collision_detection::point::shared::setter::check_throw
    Check shared_ptr and
    set coordinate value if it is not nullptr,
    otherwise throw exception std::invalid_argument.

---
+++ include/agate_pris/noer/collision_detection/point/shared/setter/check_throw.hpp
@@ -0,0 +1,47 @@
+
+#ifndef AGATE_PRIS_NOER_COLLISION_DETECTION_POINT_SHARED_SETTER_CHECK_THROW_HPP
+#define AGATE_PRIS_NOER_COLLISION_DETECTION_POINT_SHARED_SETTER_CHECK_THROW_HPP
+
+#include <cstddef>
+#include <memory>
+#include <stdexcept>
+#include <boost/geometry/core/access.hpp>
+#include <boost/geometry/core/coordinate_type.hpp>
+
+namespace agate_pris {
+namespace noer {
+namespace collision_detection {
+namespace point {
+namespace shared {
+namespace setter {
+
+// check shared_ptr and
+// set coordinate value if it is not nullptr,
+// otherwise throw exception std::invalid_argument.
+struct check_throw
+{
+    template< typename Point, std::size_t Dimension >
+    static void impl( std::shared_ptr< Point > const& r, typename boost::geometry::coordinate_type< Point >::type const& v );
+};
+
+template< typename Point, std::size_t Dimension >
+inline void check_throw::impl
+( std::shared_ptr< Point > const& r, typename boost::geometry::coordinate_type< Point >::type const& v )
+{
+    if( r )
+        boost::geometry::set< Dimension >( *r, v );
+    throw std::invalid_argument(
+        "agate_pris::noer::collision_detection::point::shared::setter::check_throw::impl "
+        "detect invalid argument.\n"
+        "argument r must not be nullptr."
+    );
+}
+
+} // setter
+} // shared
+} // point
+} // collision_detection
+} // noer
+} // agate_pris
+
+#endif
