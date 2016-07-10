c3891853a336a73f814fd16e090858dad777d9e2
agate-pris <agate-pris@users.noreply.github.com>
2016-07-10 19:40:57 +0900

    Add class collision_detection::point::shared::getter::no_check to get coordinate value from shared::entity

---
+++ include/agate_pris/noer/collision_detection/point/shared/getter/no_check.hpp
@@ -0,0 +1,40 @@
+﻿
+#ifndef AGATE_PRIS_NOER_COLLISION_DETECTION_POINT_SHARED_GETTER_NO_CHECK_HPP
+#define AGATE_PRIS_NOER_COLLISION_DETECTION_POINT_SHARED_GETTER_NO_CHECK_HPP
+
+#include <cstddef>
+#include <memory>
+#include <boost/geometry/core/access.hpp>
+#include <boost/geometry/core/coordinate_type.hpp>
+
+namespace agate_pris {
+namespace noer {
+namespace collision_detection {
+namespace point {
+namespace shared {
+namespace getter {
+
+// do not check if shared_ptr is nullptr
+// and get coordinate value
+struct no_check
+{
+    template< typename Point, std::size_t Dimension >
+    static typename boost::geometry::coordinate_type< Point >::type
+    impl( std::shared_ptr< Point > const& r );
+};
+
+template< typename Point, std::size_t Dimension >
+inline typename boost::geometry::coordinate_type< Point >::type
+no_check::impl( std::shared_ptr< Point > const& r )
+{
+    return boost::geometry::get< Dimension >( *r );
+}
+
+} // getter
+} // shared
+} // point
+} // collision_detection
+} // noer
+} // agate_pris
+
+#endif // AGATE_PRIS_NOER_COLLISION_DETECTION_POINT_SHARED_GETTER_NOCHECK_HPP
