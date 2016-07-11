c7462329685556bda662e9525af28499d03c8701
agate-pris <agate-pris@users.noreply.github.com>
2016-07-12 01:29:19 +0900

    Add class collision_detection::point::shared::setter::check_assert
    Check shared_ptr and
    set coordinate value if it is not nullptr,
    otherwise assert.

---
+++ include/agate_pris/noer/collision_detection/point/shared/setter/check_assert.hpp
@@ -0,0 +1,43 @@
+
+#ifndef AGATE_PRIS_NOER_COLLISION_DETECTION_POINT_SHARED_SETTER_CHECK_ASSERT_HPP
+#define AGATE_PRIS_NOER_COLLISION_DETECTION_POINT_SHARED_SETTER_CHECK_ASSERT_HPP
+
+#include <cstddef>
+#include <memory>
+#include <cassert>
+#include <boost/geometry/core/access.hpp>
+#include <boost/geometry/core/coordinate_type.hpp>
+
+namespace agate_pris {
+namespace noer {
+namespace collision_detection {
+namespace point {
+namespace shared {
+namespace setter {
+
+// check shared_ptr and
+// set coordinate value if it is not nullptr,
+// otherwise assert.
+struct check_assert
+{
+    template< typename Point, std::size_t Dimension >
+    static void impl( std::shared_ptr< Point > const& r, typename boost::geometry::coordinate_type< Point >::type const& v );
+};
+
+template< typename Point, std::size_t Dimension >
+inline void check_assert::impl
+( std::shared_ptr< Point > const& r, typename boost::geometry::coordinate_type< Point >::type const& v )
+{
+    auto const check_assert_impl_argument_r_must_not_be_nullptr = static_cast< bool >( r );
+    assert( check_assert_impl_argument_r_must_not_be_nullptr );
+    boost::geometry::set< Dimension >( *r, v );
+}
+
+} // setter
+} // shared
+} // point
+} // collision_detection
+} // noer
+} // agate_pris
+
+#endif // AGATE_PRIS_NOER_COLLISION_DETECTION_POINT_SHARED_SETTER_CHECK_THROW_HPP
