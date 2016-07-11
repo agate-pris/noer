dfacfbec16760b0e0108ead8f701b1b0ada34cc7
agate-pris <agate-pris@users.noreply.github.com>
2016-07-12 01:04:32 +0900

    Add class collision_detection::point::shared::setter::no_check
    Do not check if shared_ptr is nullptr
    set coordinate value.

---
+++ include/agate_pris/noer/collision_detection/point/shared/setter/no_check.hpp
@@ -0,0 +1,39 @@
+﻿
+#ifndef AGATE_PRIS_NOER_COLLISION_DETECTION_POINT_SHARED_SETTER_NO_CHECK_HPP
+#define AGATE_PRIS_NOER_COLLISION_DETECTION_POINT_SHARED_SETTER_NO_CHECK_HPP
+
+#include <cstddef>
+#include <memory>
+#include <boost/geometry/core/access.hpp>
+#include <boost/geometry/core/coordinate_type.hpp>
+
+namespace agate_pris {
+namespace noer {
+namespace collision_detection {
+namespace point {
+namespace shared {
+namespace setter {
+
+// do not check if shared_ptr is nullptr
+// set coordinate value
+struct no_check
+{
+    template< typename Point, std::size_t Dimension >
+    static void impl( std::shared_ptr< Point > const& r, typename boost::geometry::coordinate_type< Point >::type const& v );
+};
+
+template< typename Point, std::size_t Dimension >
+inline void no_check::impl
+( std::shared_ptr< Point > const& r, typename boost::geometry::coordinate_type< Point >::type const& v )
+{
+    boost::geometry::set< Dimension >( *r, v );
+}
+
+} // setter
+} // shared
+} // point
+} // collision_detection
+} // noer
+} // agate_pris
+
+#endif // AGATE_PRIS_NOER_COLLISION_DETECTION_POINT_SHARED_SETTER_CHECK_THROW_HPP
