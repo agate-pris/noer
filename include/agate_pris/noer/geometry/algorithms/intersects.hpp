f0bd9772f5757c62b48de9e13e0d15f9460c256e
agate-pris <agate-pris@users.noreply.github.com>
2016-07-20 21:38:03 +0900

    rename intersects files
    remove prefix intersetcs_

--- include/agate_pris/noer/geometry/algorithms/intersects.hpp
+++ include/agate_pris/noer/geometry/algorithms/intersects.hpp
@@ -3,16 +3,16 @@
 #define AGATE_PRIS_NOER_COLLISION_DETECTION_INTERSECTS_HPP
 
 #include <agate_pris/noer/geometry/core/tag.hpp>
-#include <agate_pris/noer/geometry/algorithms/intersects/intersects_box_box.hpp>
-#include <agate_pris/noer/geometry/algorithms/intersects/intersects_box_point.hpp>
-#include <agate_pris/noer/geometry/algorithms/intersects/intersects_box_segment.hpp>
-#include <agate_pris/noer/geometry/algorithms/intersects/intersects_box_triangle.hpp>
-#include <agate_pris/noer/geometry/algorithms/intersects/intersects_container.hpp>
-#include <agate_pris/noer/geometry/algorithms/intersects/intersects_container_polymorphic.hpp>
-#include <agate_pris/noer/geometry/algorithms/intersects/intersects_point_triangle.hpp>
-#include <agate_pris/noer/geometry/algorithms/intersects/intersects_segment_segment.hpp>
-#include <agate_pris/noer/geometry/algorithms/intersects/intersects_segment_triangle.hpp>
-#include <agate_pris/noer/geometry/algorithms/intersects/intersects_triangle_triangle.hpp>
+#include <agate_pris/noer/geometry/algorithms/intersects/box_box.hpp>
+#include <agate_pris/noer/geometry/algorithms/intersects/box_point.hpp>
+#include <agate_pris/noer/geometry/algorithms/intersects/box_segment.hpp>
+#include <agate_pris/noer/geometry/algorithms/intersects/box_triangle.hpp>
+#include <agate_pris/noer/geometry/algorithms/intersects/container.hpp>
+#include <agate_pris/noer/geometry/algorithms/intersects/container_polymorphic.hpp>
+#include <agate_pris/noer/geometry/algorithms/intersects/point_triangle.hpp>
+#include <agate_pris/noer/geometry/algorithms/intersects/segment_segment.hpp>
+#include <agate_pris/noer/geometry/algorithms/intersects/segment_triangle.hpp>
+#include <agate_pris/noer/geometry/algorithms/intersects/triangle_triangle.hpp>
 
 namespace agate_pris {
 namespace noer {
