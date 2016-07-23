322cb66ca8a2be4f3a2a41b9f7968cc23cdc8b82
agate-pris <agate-pris@users.noreply.github.com>
2016-07-23 22:18:56 +0900

    Adjust indent and space of point_order.hpp
    Adjust indent and space of geometry/core/point_order.hpp .

--- include/agate_pris/noer/geometry/core/point_order.hpp
+++ include/agate_pris/noer/geometry/core/point_order.hpp
@@ -4,28 +4,28 @@
 
 #include <boost/geometry/core/point_order.hpp>
 
-namespace agate_pris
-{
-	namespace noer
-	{
-		namespace geometry
-		{
-			namespace traits
-			{
-				template< typename CollisionDetectionType >
-				struct point_order
-				{
-					static const boost::geometry::order_selector value = boost::geometry::point_order< CollisionDetectionType >::value;
-				};
-			}
+namespace agate_pris {
+namespace noer {
+namespace geometry {
 
-			template< typename CollisionDetectionType >
-			struct point_order
-			{
-				static const boost::geometry::order_selector value = traits::point_order< CollisionDetectionType >::value;
-			};
-		}
-	}
-}
+namespace traits {
+
+template< typename CollisionDetectionType >
+struct point_order
+{
+    static const boost::geometry::order_selector value = boost::geometry::point_order< CollisionDetectionType >::value;
+};
+
+} // traits
+
+template< typename CollisionDetectionType >
+struct point_order
+{
+    static const boost::geometry::order_selector value = traits::point_order< CollisionDetectionType >::value;
+};
+
+} // geometry
+} // noer
+} // agate_pris
 
 #endif // AGATE_PRIS_NOER_GEOMETRY_POINT_ORDER_HPP
