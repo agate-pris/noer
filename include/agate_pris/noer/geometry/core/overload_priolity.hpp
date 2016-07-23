d927d976cdf8fa0bb407d9db0fa469cb0ab1b906
agate-pris <agate-pris@users.noreply.github.com>
2016-07-23 17:54:58 +0900

    Add overload priolity for geometry.
    Add file noer/geometry/core/overload_priolity.hpp
    Add class geometry::overload_priolity
    Add class geometry::traits::overload_priolity
    Specialize geometry::traits::overload_priolity and it is used as overload resolution priolity.

---
+++ include/agate_pris/noer/geometry/core/overload_priolity.hpp
@@ -0,0 +1,29 @@
+﻿
+#ifndef AGATE_PRIS_NOER_GEOMETRY_CORE_OVERLOAD_PRIOLITY_HPP
+#define AGATE_PRIS_NOER_GEOMETRY_CORE_OVERLOAD_PRIOLITY_HPP
+
+#include <boost/mpl/long.hpp>
+
+namespace agate_pris {
+namespace noer {
+namespace geometry {
+
+namespace traits {
+
+template< typename T >
+struct overload_priolity
+    : boost::mpl::long_< 0 >
+{};
+
+} // traits
+
+template< typename T >
+struct overload_priolity
+    : traits::overload_priolity< T >
+{};
+
+} // geometry
+} // noer
+} // agate_pris
+
+#endif // AGATE_PRIS_NOER_GEOMETRY_CORE_OVERLOAD_PRIOLITY_HPP
