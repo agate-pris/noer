dbc19e29a25a57abe76871906d03735ecfc7bdf6
agate-pris <agate-pris@users.noreply.github.com>
2016-07-23 22:22:46 +0900

    Adjust indent and space of point_type.hpp
    Adjust indent and space of geometry/core/point_type.hpp .

--- include/agate_pris/noer/geometry/core/point_type.hpp
+++ include/agate_pris/noer/geometry/core/point_type.hpp
@@ -4,30 +4,31 @@
 
 #include <boost/geometry/core/point_type.hpp>
 
-namespace agate_pris
-{
-	namespace noer
-	{
-		namespace geometry
-		{
-			namespace traits
-			{
-				template< typename CollisionDetection >
-				struct point_type
-				{
-					using type = typename boost::geometry::point_type< CollisionDetection >::type;
-				};
-			}
+namespace agate_pris {
+namespace noer {
+namespace geometry {
 
-			template< typename CollisionDetectionType >
-			struct point_type
-			{
-				using type = typename traits::point_type< CollisionDetectionType >::type;
-			};
-			template< typename CollisionDetectionType >
-			using point_type_t = typename point_type< CollisionDetectionType >::type;
-		}
-	}
-}
+namespace traits {
+
+template< typename CollisionDetection >
+struct point_type
+{
+    using type = typename boost::geometry::point_type< CollisionDetection >::type;
+};
+
+} // traits
+
+template< typename CollisionDetectionType >
+struct point_type
+{
+    using type = typename traits::point_type< CollisionDetectionType >::type;
+};
+
+template< typename CollisionDetectionType >
+using point_type_t = typename point_type< CollisionDetectionType >::type;
+
+} // geometry
+} // noer
+} // agate_pris
 
 #endif // AGATE_PRIS_NOER_GEOMETRY_POINT_TYPE_HPP
