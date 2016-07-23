8c1f7f642fbc34b244ded6815a49285a4ec6d329
agate-pris <agate-pris@users.noreply.github.com>
2016-07-23 22:23:37 +0900

    Adjust indent and space of tag.hpp
    Adjust indent and space of geometry/core/tag.hpp .

--- include/agate_pris/noer/geometry/core/tag.hpp
+++ include/agate_pris/noer/geometry/core/tag.hpp
@@ -4,31 +4,31 @@
 
 #include <boost/geometry/core/tag.hpp>
 
-namespace agate_pris
+namespace agate_pris {
+namespace noer {
+namespace geometry {
+
+namespace traits {
+
+template< typename CollisionDetection >
+struct tag
 {
-	namespace noer
-	{
-		namespace geometry
-		{
-			namespace traits
-			{
-				template< typename CollisionDetection >
-				struct tag
-				{
-					using type = typename boost::geometry::tag< CollisionDetection >::type;
-				};
-			}
+    using type = typename boost::geometry::tag< CollisionDetection >::type;
+};
 
-			template< typename CollisionDetection >
-			struct tag
-			{
-				using type = typename traits::tag< CollisionDetection >::type;
-			};
+} // traits
 
-			template< typename CollisionDetection >
-			using tag_t = typename tag< CollisionDetection >::type;
-		}
-	}
-}
+template< typename CollisionDetection >
+struct tag
+{
+    using type = typename traits::tag< CollisionDetection >::type;
+};
+
+template< typename CollisionDetection >
+using tag_t = typename tag< CollisionDetection >::type;
+
+} // agate_pris
+} // noer
+} // geometry
 
 #endif // AGATE_PRIS_NOER_GEOMETRY_TAG_HPP
